5654be8efe81a1674fcbdbb1a510c001a416052b
Michael Vogt <michael.vogt@progwise.net>
2021-05-09 17:54:20 +0200

    itemTable

---
+++ frontend/pages/projects.tsx
@@ -0,0 +1,14 @@
+import {useEffect, useState} from 'react';
+
+const Projects = () => {
+  useState()
+  useEffect(() => {
+
+  })
+  return <article>
+    <h2>Your projects</h2>
+
+  </article>
+}
+
+export default Projects
