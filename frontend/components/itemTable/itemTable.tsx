3f4fe960ad4bbb31d8a39a3e21e1534411a7ebca
Yohannes <johnbrhane1@gmail.com>
2021-05-11 13:55:11 +0200

    table design

--- frontend/components/itemTable/itemTable.tsx
+++ frontend/components/itemTable/itemTable.tsx
@@ -1,69 +1,91 @@
+export enum SortDirection {
+  ASC,
+  DESC,
+}
+
 export interface IItemTableProps {
-  columns: [{
-    title: string
-    value: (IItemTableItem) => string | JSX.Element
-    orderedBy: 'asc' | 'desc' | undefined
-    onClick: (() => void) | undefined
-  }]
-  items: [IItemTableItem]
+  columns: Array<{
+    title: string;
+    value: (item: any) => string | JSX.Element;
+    orderedBy?: SortDirection;
+    onClick?: () => void;
+  }>;
+
+  items: Array<IItemTableItem>;
   page: {
-    totalItemCount: number
-    firstItemIndex: number
-    onPrevious: () => void
-    onFirst: () => void
-    onNext: () => void
-    onLast: () => void
-  }
+    totalItemCount: number;
+    firstItemIndex: number;
+    itemsPerPage: number;
+    onPrevious: () => void;
+    onFirst: () => void;
+    onNext: () => void;
+    onLast: () => void;
+  };
 }
 
-export interface IItemTableItem {
-
-}
+export interface IItemTableItem {}
 
 export const ItemTable = (props: IItemTableProps) => {
-  return <div>
-    <table>
-      <thead>
-      <tr>
-        {props.columns.map(c =>
-          <th>{c.title}</th>
-        )}
-      </tr>
-      </thead>
-      <tbody>
-      {props.items.map(item =>
-        <tr>
-          {
-            props.columns.map(c =>
-              <td onClick={c.onClick}>
-                {c.value(item)}
-              </td>
-            )
-          }
-        </tr>
-      )}
-      </tbody>
-      <tfoot>
-      <tr>
-        <td colSpan={props.columns.length}>
-          <button onClick={props.page.onFirst}>
-            first
-          </button>
-          <button onClick={props.page.onPrevious}>
-            previous
-          </button>
-          <span>
-            item {props.page.firstItemIndex} - {props.page.firstItemIndex + props.items.length} / {props.page.totalItemCount}
-          </span>
-          <button onClick={props.page.onNext}>
-            next
-          </button>
-          <button onClick={props.page.onLast}>
-            last
-          </button>
-        </td>
-      </tr>
-      </tfoot>
-    </table>
-  </div>
-}
+  return (
+    <div>
+      <table className="shadow-lg bg-white w-full">
+        <thead>
+          <tr>
+            {props.columns.map((c) => (
+              <th className="bg-blue-100 border text-left px-2 pb-4">
+                {c.title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="before:space-y-4">
+          {props.items.map((item, rowIndex) => (
+            <tr key={rowIndex}>
+              {props.columns.map((c, colIndex) => (
+                <td
+                  className="py-2 border-b-2 px-2 "
+                  key={colIndex}
+                  onClick={c.onClick}
+                >
+                  {c.value(item)}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td
+              className="bg-blue-100 border text-right px-1 pt-4 pb-1"
+              colSpan={props.columns.length}
+            >
+              <button
+                className="btn btn-blue mr-3"
+                onClick={props.page.onFirst}
+              >
+                first
+              </button>
+              <button
+                className="btn btn-blue mr-3"
+                onClick={props.page.onPrevious}
+              >
+                previous
+              </button>
+              <span>
+                item {props.page.firstItemIndex} -{" "}
+                {props.page.firstItemIndex + props.items.length} /{" "}
+                {props.page.totalItemCount}
+              </span>
+              <button className="btn btn-blue ml-3" onClick={props.page.onNext}>
+                next
+              </button>
+              <button className="btn btn-blue ml-3" onClick={props.page.onLast}>
+                last
+              </button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+};
