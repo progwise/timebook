848de88a7ba180706b24a4f219a63b85573c7e95
Michael Vogt <michael.vogt@progwise.net>
2021-07-06 12:08:04 +0200

    upgrade packages

--- backend/graphql/schema/generated/nexus-typegen.ts
+++ backend/graphql/schema/generated/nexus-typegen.ts
@@ -4,7 +4,7 @@
  */
 
 
-import { IContext } from "./../../context"
+import type { IContext } from "./../../context"
 
 
 
@@ -127,6 +127,8 @@ export interface NexusGenTypes {
 
 declare global {
   interface NexusGenPluginTypeConfig<TypeName extends string> {
+  }
+  interface NexusGenPluginInputTypeConfig<TypeName extends string> {
   }
   interface NexusGenPluginFieldConfig<TypeName extends string, FieldName extends string> {
   }
