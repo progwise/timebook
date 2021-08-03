b8002fdd93c3c02c28e5588fc18879c0eadd62fd
Michael Vogt <michael.vogt@progwise.net>
2021-08-03 12:51:49 +0200

    ignore dist

--- backend/dist/graphql/context.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-var prisma = new PrismaClient();
-export var context = {
-    prisma: prisma
-};
-//# sourceMappingURL=context.js.map
--- backend/dist/graphql/server.js
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from "apollo-server";
-import { context } from "./context";
-import { schema } from "./schema/schema";
-var server = new ApolloServer({ schema: schema, context: context });
-server.listen().then(function (_a) {
-  var url = _a.url;
-  console.log("Apollo Server ready at " + url);
-});
-//# sourceMappingURL=server.js.map
