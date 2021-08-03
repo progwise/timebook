a3d63dd995f4f6367753ca3ce94a7d3e5de2c186
Yohannes <johnbrhane1@gmail.com>
2021-08-03 14:27:07 +0200

    tests implemeted

--- frontend/components/calendarIcon.tsx
+++ frontend/components/calendarIcon.tsx
@@ -16,17 +16,16 @@ const CalendarIcon = (props: ICalendarIconProps): JSX.Element => {
     }
 
     const childPosition: CalendarIconChildPosition = props.childPosition || 'right'
-
     let classNames = props.className
 
     if (props.onClick) {
-        classNames = classNames + ' opacity-60 hover:opacity-100 cursor-pointer'
+        classNames = classNames + ' opacity-60 hover:opacity-100'
     }
 
     return (
-        <div className="flex items-center">
+        <div title="Calendar icon" onClick={handleClick} className="flex items-center cursor-pointer">
             {props.children && childPosition === 'left' && props.children}
-            <img onClick={handleClick} className={classNames} src={props.src} />
+            <img className={classNames} src={props.src} />
             {props.children && childPosition === 'right' && props.children}
         </div>
     )
