8ea7c4137d1a215d0756f63a8504066e801d71e4
Michael Vogt <michael.vogt@progwise.net>
2021-08-03 16:12:59 +0200

    images next config repair

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withImages = require('next-images')
-module.exports = withImages()
+const config = withImages()
 module.exports = {
+    ...config,
     eslint: {
         dirs: ['pages', 'hooks', 'components'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
     },
