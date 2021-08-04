028a3a1eae2347c0fc04bf7bf4b5b9601c2cadd9
Michael Vogt <michael.vogt@progwise.net>
2021-08-04 15:44:31 +0200

    fixed backend

--- backend/README.md
+++ backend/README.md
@@ -7,10 +7,23 @@ docker build --tag timebook-graphql .
 docker run -p 4000:4000 timebook-graphql
 ```
 
-## run docker-compose
+## run all in docker-compose
 
 ```
 docker-compose up
 ```
 
 you can ignore seeding error messages because of unique constraint violations for the moment
+
+## run db in docker and graphql in dev mode
+
+```
+docker-compose -f docker-compose.dev.yml up
+```
+
+plus
+
+```
+npm i
+npm run dev
+```
--- frontend/pages/projects.tsx
+++ frontend/pages/projects.tsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from "react";
-import {
-  ItemTable,
-  SortDirection,
-} from "../components/itemTable/itemTable";
-import {useRouter} from 'next/router';
-import {IProject, useProjects} from '../hooks/useProjects';
-
+import { ItemTable, SortDirection } from "../components/itemTable/itemTable";
+import { useRouter } from "next/router";
+import { IProject, useProjects } from "../hooks/useProjects";
 
 const Projects = () => {
+  const { projects } = useProjects();
+  const router = useRouter();
 
-  const { projectList } = useProjects()
-  const router = useRouter()
-
-  const [itemsPerPage, setItemsPerPage] = useState(10)
-  const [firstItemIndex, setFirstItemIndex] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [firstItemIndex, setFirstItemIndex] = useState(1);
 
   const tableColumns = [
     {
@@ -23,63 +18,67 @@ const Projects = () => {
     },
     {
       title: "Duration",
-      value: (item: IProject) =>
-        `${item.startDate?.toLocaleDateString()}-${item.endDate?.toLocaleDateString()}`,
+      value: (item: IProject) => `${item.startDate?.toLocaleDateString()}-${item.endDate?.toLocaleDateString()}`,
       onClick: () => {},
       orderedBy: SortDirection.DESC,
     },
     {
-      title: '',
-      value: (item: IProject) => <span className="float-right">
-          <button className="btn btn-gray2 mr-3" onClick={() => handleDeleteProject(item)}>Delete</button>
-          <button className="btn btn-gray2" onClick={() => handleProjectDetails(item)}>Details</button>
+      title: "",
+      value: (item: IProject) => (
+        <span className="float-right">
+          <button className="btn btn-gray2 mr-3" onClick={() => handleDeleteProject(item)}>
+            Delete
+          </button>
+          <button className="btn btn-gray2" onClick={() => handleProjectDetails(item)}>
+            Details
+          </button>
         </span>
-    }
+      ),
+    },
   ];
 
   const handleProjectDetails = async (project: IProject) => {
-    await router.push(`/projects/${project.id}`)
-  }
+    await router.push(`/projects/${project.id}`);
+  };
 
   const handleDeleteProject = (project: IProject) => {
     // setProjectList(projectList.filter(p => p.id !== project.id))
-  }
+  };
 
   const handleAddProject = async () => {
-    await router.push('/projects/new')
-  }
+    await router.push("/projects/new");
+  };
 
   return (
     <article>
       <h2 className="flex justify-between">
         <span>Your projects</span>
         <span>
-          <button className="btn btn-gray1" onClick={handleAddProject}>Add</button>
+          <button className="btn btn-gray1" onClick={handleAddProject}>
+            Add
+          </button>
         </span>
       </h2>
-      {!projectList.data
-        ? <div>loading</div>
-        :
+      {!projects ? (
+        <div>loading</div>
+      ) : (
         <ItemTable
           columns={tableColumns}
-          items={projectList.data.projects}
+          items={projects}
           itemClick={handleProjectDetails}
           page={{
             totalItemCount: 521,
             firstItemIndex: firstItemIndex,
             itemsPerPage: itemsPerPage,
-            onPrevious: () => {
-            },
-            onFirst: () => {
-            },
+            onPrevious: () => {},
+            onFirst: () => {},
             onNext: () => {
-              setFirstItemIndex(firstItemIndex + itemsPerPage)
+              setFirstItemIndex(firstItemIndex + itemsPerPage);
             },
-            onLast: () => {
-            },
+            onLast: () => {},
           }}
         />
-      }
+      )}
     </article>
   );
 };
