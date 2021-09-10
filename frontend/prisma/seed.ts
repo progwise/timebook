7b0e02ebf9b93c66b4f7edfa3ee05e69c6e22552
Pascal Helbig <PascalHelbig@users.noreply.github.com>
2021-09-10 15:29:47 +0200

    refactor(backend): use next api routes for the backend (#47)
    * refactor(backend): use next api routes for the backend
    
    * style: run prettier

---
+++ frontend/prisma/seed.ts
@@ -0,0 +1,57 @@
+/* eslint-disable unicorn/no-process-exit */
+/* eslint-disable no-console */
+
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const userData: Prisma.UserCreateInput[] = [
+    {
+        name: 'Seeded User 1',
+        email: 'seeded.user1@timebook.progfiles.net',
+        projects: {
+            create: [
+                {
+                    title: ' Seeded Project 1',
+                },
+                {
+                    title: 'Seeded Project 2',
+                },
+            ],
+        },
+    },
+    {
+        name: 'Seeded User 2',
+        email: 'seeded.user2@timebook.progfiles.net',
+        projects: {
+            create: [
+                {
+                    title: ' Seeded Project 3',
+                },
+                {
+                    title: 'Seeded Project 4',
+                },
+            ],
+        },
+    },
+]
+
+async function main() {
+    console.log(`Start seeding ...`)
+    for (const u of userData) {
+        const user = await prisma.user.create({
+            data: u,
+        })
+        console.log(`Created user with id: ${user.id}`)
+    }
+    console.log(`Seeding finished.`)
+}
+
+main()
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
