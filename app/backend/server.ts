a5958a9c096ca31a7779e4f39e551bf91cc8080f
Pascal Helbig <PascalHelbig@users.noreply.github.com>
2021-09-17 16:13:26 +0200

    refactor: change folder structure (#49)
    * refactor(backend): clean schema.ts
    
    * feat(backend): add createWorkHour mutation
    
    * refactor: change folder structure
    
    * ci: fix GitHub actions
    
    * ci: fix prisma client generation
    
    * micro dep
    
    * npm 7
    
    Co-authored-by: Michael Vogt <michael.vogt@progwise.net>

--- app/backend/server.ts
+++ app/backend/server.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from 'apollo-server-micro'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
-import { schema } from './schema'
-import { context } from './context'
+import { schema } from './graphql/schema'
+import { context } from './graphql/context'
 
 export const server = new ApolloServer({ schema, context, plugins: [ApolloServerPluginLandingPageGraphQLPlayground] })
