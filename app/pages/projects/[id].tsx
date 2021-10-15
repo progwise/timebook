2ce972f9fd3fd8ec2db97e7ce7fa2dfdc76f42cb
Yohannes <johnbrhane1@gmail.com>
2021-10-15 10:52:02 +0200

    updated input validation

--- app/pages/projects/[id].tsx
+++ app/pages/projects/[id].tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { IProject, useProjects } from '../../frontend/hooks/useProjects'
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 
 
 const now = new Date()
@@ -12,12 +12,9 @@ const newProject: IProject = {
     endDate: new Date(now.getFullYear(), 12, 31),
 }
 
-
-
 const ProjectDetails = (): JSX.Element => {
     const { projects } = useProjects()
     const [currentProject, setCurrentProject] = useState<IProject>(() => newProject)
-
 
     const {register, handleSubmit, formState: {errors}} = useForm()
     const onSubmit = async (data: any) => {
@@ -25,12 +22,11 @@ const ProjectDetails = (): JSX.Element => {
       await router.push('/projects');
     }
 
-
     const router = useRouter()
     const { id } = router.query
     
     const handleCancel = async () => {
-        await router.push('/projects')
+          await router.push('/projects')
     }
 
     useEffect(() => {
@@ -44,7 +40,7 @@ const ProjectDetails = (): JSX.Element => {
 
     return (
         <article>
-            <form key={currentProject.id} onSubmit = {handleSubmit(onSubmit)}>
+            <form key={currentProject.id} onSubmit={handleSubmit(onSubmit)}>
                 {isNewProject() ? <h2>Create Project</h2> : <h2>Edit Project</h2>}
                 <label className="text-gray-500">
                     <span>Id</span>
@@ -52,7 +48,7 @@ const ProjectDetails = (): JSX.Element => {
                 </label>
                 <label className="text-gray-500">
                     <span>Name</span>
-                    <input type="text" defaultValue={currentProject.title} {...register("Name", { required: true })}/>
+                    <input type="text" defaultValue={currentProject.title} {...register('Name', { required: true })}/>
                     {errors.Name && <span>This field is required</span>}
                 </label>
                 <label>
