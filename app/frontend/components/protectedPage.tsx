0db7763bbd33d9471cf4f730e5c1e1e783232ee1
Pascal Helbig <pascal.helbig@progwise.net>
2021-10-29 14:05:42 +0200

    feat(auth): mark all (except home) pages as protected

---
+++ app/frontend/components/protectedPage.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react'
+import { signIn, useSession } from 'next-auth/react'
+
+interface ProtectedPageProps {
+    children?: ReactNode
+}
+
+export const ProtectedPage = (props: ProtectedPageProps): JSX.Element => {
+    const session = useSession({
+        required: true,
+        onUnauthenticated: () => signIn('github'),
+    })
+
+    if (session.status === 'authenticated') {
+        return <>{props.children}</>
+    }
+
+    return <></>
+}
