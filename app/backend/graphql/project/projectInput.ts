e8fc22beaf895214f8d3a6436e72261deb52eb81
Pascal Helbig <pascal.helbig@progwise.net>
2021-11-01 17:06:22 +0100

    feat(graphql): add projectCreate, projectUpdate and projectDelete mutations

---
+++ app/backend/graphql/project/projectInput.ts
@@ -0,0 +1,10 @@
+import { inputObjectType } from 'nexus'
+
+export const ProjectInput = inputObjectType({
+    name: 'ProjectInput',
+    definition: (t) => {
+        t.string('title')
+        t.nullable.date('start')
+        t.nullable.date('end')
+    },
+})
