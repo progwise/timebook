76eeef6faf625124fbb48ad53948423aae20d5ee
Pascal Helbig <pascal.helbig@progwise.net>
2021-11-01 14:35:29 +0100

    feat(prisma): add membership table

---
+++ app/backend/prisma/migrations/20211101134009_add_membership_table/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorId` on the `Project` table. All the data in the column will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'MEMBER');
+
+-- DropForeignKey
+ALTER TABLE "Project" DROP CONSTRAINT "Project_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Project" DROP COLUMN "authorId";
+
+-- CreateTable
+CREATE TABLE "Membership" (
+    "invitedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "inviteAcceptedAt" TIMESTAMP(3),
+    "userId" TEXT NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "role" "Role" NOT NULL DEFAULT E'MEMBER',
+
+    PRIMARY KEY ("userId","projectId")
+);
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
