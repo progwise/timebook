63c7315fc163683c7b7376f7b51d71fc57f843bf
Yohannes <johnbrhane1@gmail.com>
2021-11-12 16:32:33 +0100

    Project update implemented

--- app/pages/projects/[id].tsx
+++ app/pages/projects/[id].tsx
@@ -1,18 +1,26 @@
 import { useRouter } from 'next/router'
 import { ProjectForm } from '../../frontend/components/projectForm/projectForm'
 import { ProtectedPage } from '../../frontend/components/protectedPage'
-import { ProjectInput, useProjectsQuery } from '../../frontend/generated/graphql'
+import { ProjectInput, useProjectsQuery, useProjectUpdateMutation } from '../../frontend/generated/graphql'
 
 const ProjectDetails = (): JSX.Element => {
   const [{ data, fetching }] = useProjectsQuery()
   const router = useRouter()
   const { id } = router.query
   const selectedProject = data?.projects.find((p) => p.id === id)
+  const [, projectUpdate] = useProjectUpdateMutation()
 
   const handleSubmit = async (data: ProjectInput) => {
-    // eslint-disable-next-line no-console
-    console.log(data)
-    await router.push('/projects')
+    try {
+      if (!selectedProject?.id) {
+        throw new Error('project id missing')
+      }
+      const result = await projectUpdate({ id: selectedProject.id, data })
+      if (result.error) {
+        throw new Error('graphql error')
+      }
+      await router.push('/projects')
+    } catch {}
   }
 
   const handleCancel = async () => {
