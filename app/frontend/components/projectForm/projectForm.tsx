25d2ba41ffd15e0cdf2e2a7d62c692ef34ab6666
Slash52 <linusvogt@Linus-Air.fritz.box>
2021-11-12 16:51:43 +0100

    review comment

--- app/frontend/components/projectForm/projectForm.tsx
+++ app/frontend/components/projectForm/projectForm.tsx
@@ -24,8 +24,6 @@ export const ProjectForm = (props: ProjectFormProps): JSX.Element => {
     },
   })
 
-  console.log(project)
-
   const handleSubmitHelper = (data: ProjectInput) => {
     return onSubmit({
       ...data,
