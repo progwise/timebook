a85f80631d5c2fd23630f1c3b2d542f6c6f3ac54
Slash52 <91463734+Slash52@users.noreply.github.com>
2021-11-19 17:05:15 +0100

    fix esLint error

--- app/frontend/components/button/button.tsx
+++ app/frontend/components/button/button.tsx
@@ -3,9 +3,10 @@ interface ButtonProps {
   type?: 'button' | 'submit'
   variant: 'primary' | 'secondary'
   onClick?: () => void
+  children: React.ReactNode
 }
 
-export const Button: React.FC<ButtonProps> = ({ children, disabled, type = 'button', variant, onClick }) => {
+export const Button = ({ children, disabled, type = 'button', variant, onClick }: ButtonProps): JSX.Element => {
   const classNamePrimary = 'bg-blue-500 hover:bg-blue-700 disabled:bg-blue-500'
   const classNameSecondary = 'bg-gray-400 hover:bg-gray-500 disabled:bg-gray-400'
   return (
