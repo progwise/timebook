ea3c00943554b3754e6f678310d37ffdceeffe2a
Slash52 <91463734+Slash52@users.noreply.github.com>
2021-11-26 14:56:37 +0100

    Team List

---
+++ app/pages/team.tsx
@@ -0,0 +1,33 @@
+import { Button } from '../frontend/components/button/button'
+
+const Team = (): JSX.Element => {
+  return (
+    <article>
+      <h2 className="flex justify-between">
+        <span>Your projects</span>
+      </h2>
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Projects</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Linus</td>
+            <td>linus@xyz.de</td>
+            <td>Projekt 1, Projekt 2</td>
+            <td>
+              <Button variant="primarySlim">Details</Button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </article>
+  )
+}
+
+export default Team
