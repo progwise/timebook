4057d99c47156a15bc152b5b7c52034662decd35
Slash52 <91463734+Slash52@users.noreply.github.com>
2021-11-26 15:15:00 +0100

    Team List (#95)
    * Team List
    
    * fix

--- app/frontend/components/topNavigation/topNavigation.tsx
+++ app/frontend/components/topNavigation/topNavigation.tsx
@@ -10,7 +10,7 @@ export const TopNavigation = (): JSX.Element => {
       <TopNavigationLink href="/time">Time</TopNavigationLink>
       <TopNavigationLink href="/projects">Projects</TopNavigationLink>
       <TopNavigationLink href="/reports">Reports</TopNavigationLink>
-      <TopNavigationLink href="/tasks">Tasks</TopNavigationLink>
+      <TopNavigationLink href="/team">Team</TopNavigationLink>
       {session.status === 'authenticated' ? (
         <TopNavigationLink onClick={() => signOut({ callbackUrl: '/' })}>Sign out</TopNavigationLink>
       ) : (
