2377be1d49ca8fe5d52ead31c2b93f7ad7f39899
Pascal Helbig <PascalHelbig@users.noreply.github.com>
2021-11-26 13:36:12 +0100

    fix(prisma): fix to many db connection on dev (#90)

--- app/backend/graphql/context.ts
+++ app/backend/graphql/context.ts
@@ -8,7 +8,17 @@ export interface Context {
   session: Session | null
 }
 
-const prisma = new PrismaClient()
+// from https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
+const prisma = global.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
+}
 
 export const context = async ({ req: request }: { req: NextApiRequest }): Promise<Context> => {
   const session = await getSession({ req: request })
