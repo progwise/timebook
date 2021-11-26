72e2e9283d6a407c7cf4998072e4db1c173c7f76
santeleifer <santeleifer@icloud.com>
2021-11-26 15:06:48 +0100

    Add red Button component

--- app/frontend/components/deleteProjectModal.tsx
+++ app/frontend/components/deleteProjectModal.tsx
@@ -21,25 +21,23 @@ export const DeleteProjectModal = ({ onClose, project, open }: ModalProps): JSX.
   }
 
   return (
-    <div>
-      <Dialog open={open} onClose={onClose} className="fixed inset-0 overflow-y-auto">
-        <div className="flex items-center justify-center min-h-screen">
-          <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-          <div className="relative bg-white rounded-3xl p-7 shadow-lg">
-            <Dialog.Title className="text-center text-lg">
-              Are you sure you want to delete project {project?.title}?
-            </Dialog.Title>
-            <div className="flex gap-4 pt-5 flex-wrap flex-col sm:flex-row sm:justify-end">
-              <Button variant="secondary" onClick={onClose} disabled={projectDeleteState.fetching}>
-                Cancel
-              </Button>
-              <Button variant="primary" onClick={handleDeleteProject} disabled={projectDeleteState.fetching}>
-                Delete
-              </Button>
-            </div>
+    <Dialog open={open} onClose={onClose} className="fixed inset-0 overflow-y-auto">
+      <div className="flex items-center justify-center min-h-screen">
+        <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+        <div className="relative bg-white rounded-3xl p-7 shadow-lg">
+          <Dialog.Title className="text-center text-lg">
+            Are you sure you want to delete project {project?.title}?
+          </Dialog.Title>
+          <div className="flex gap-4 pt-5 flex-wrap flex-col sm:flex-row sm:justify-end">
+            <Button variant="secondary" onClick={onClose} disabled={projectDeleteState.fetching}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={handleDeleteProject} disabled={projectDeleteState.fetching}>
+              Delete
+            </Button>
           </div>
         </div>
-      </Dialog>
-    </div>
+      </div>
+    </Dialog>
   )
 }
