be9007473ec13a2595735c14a1483d0587009af9
Pascal Helbig <pascal.helbig@progwise.net>
2021-11-27 18:21:48 +0100

    chore(package.json): remove unused dependencies

--- app/next.config.js
+++ app/next.config.js
@@ -1,12 +1,8 @@
 /* eslint-disable unicorn/prefer-module */
-/* eslint-disable @typescript-eslint/no-var-requires */
 
-const withImages = require('next-images')
-const config = withImages()
 module.exports = {
-  ...config,
   eslint: {
-    dirs: ['pages', 'hooks', 'components'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    dirs: ['pages', 'frontend', 'backend'], // Only run ESLint on the 'pages', 'frontend' and 'backend' directories during production builds (next build)
   },
   env: {
     ROOT: __dirname,
