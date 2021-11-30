9d75c303bb17c8f5cc808f3222fefa7a5524f1a6
Pascal Helbig <PascalHelbig@users.noreply.github.com>
2021-11-30 17:13:43 +0100

    refactor: cleanup calendar selector component (#108)

--- app/frontend/components/calendarSelector.tsx
+++ app/frontend/components/calendarSelector.tsx
@@ -3,53 +3,54 @@ import calendarIcon from '../assets/calendarIcon.svg'
 import backIcon from '../assets/backIcon.svg'
 import forwardIcon from '../assets/forwardIcon.svg'
 import home from '../assets/home.svg'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import CalendarIcon from './calendarIcon'
+import {
+  addMonths,
+  eachDayOfInterval,
+  endOfMonth,
+  endOfWeek,
+  format,
+  getDate,
+  isSameDay,
+  isSameMonth,
+  isWeekend,
+  startOfMonth,
+  startOfWeek,
+  subMonths,
+} from 'date-fns'
 
-export const getMonthTitle = (day: Date): string => {
-  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  const monthName = monthNames[day.getMonth()]
-  return `${monthName} ${day.getFullYear()}`
+interface DayItemProps {
+  day: Date
+  selectedDate: Date
+  onClick: () => void
 }
 
-export const getDayTitle = (day: Date): string => {
-  const monthTitle = getMonthTitle(day)
-  const dayString = day.getDate()
-  return `${dayString} ${monthTitle}`
-}
+const DayItem = ({ day, selectedDate, onClick }: DayItemProps): JSX.Element => {
+  const classNames = ['text-center border cursor-pointer p-0']
 
-const DayItem = (props: { day: Date; selectedDate: Date; onClick: (day: Date) => void }): JSX.Element => {
-  const day = props.day
-  const selectedDate = props.selectedDate
-  const dayString = day.getDate()
-
-  const classNames = new Array<string>()
-
-  classNames.push('text-center border cursor-pointer p-0')
-
-  const isSelectedYearAndMonth =
-    selectedDate.getFullYear() === day.getFullYear() && selectedDate.getMonth() === day.getMonth()
-
-  if (!isSelectedYearAndMonth) {
+  if (!isSameMonth(day, selectedDate)) {
     classNames.push('italic')
   }
 
-  let title = getDayTitle(day)
+  let title = format(day, 'do MMM yyyy')
 
-  if (selectedDate.toLocaleDateString() === day.toLocaleDateString()) {
+  if (isSameDay(day, selectedDate)) {
     classNames.push('border-red-700')
     title = `Selected Day, ${title}`
   }
 
-  if (day.getDay() === 6 || day.getDay() === 0) {
+  if (isWeekend(day)) {
     classNames.push('text-green-600')
-  } else if (isSelectedYearAndMonth) {
+  }
+
+  if (!isWeekend(day) && isSameMonth(day, selectedDate)) {
     classNames.push('font-bold')
   }
 
   return (
-    <button title={title} className={classNames.join(' ')} onClick={() => props.onClick(day)} type="button">
-      {dayString}
+    <button title={title} className={classNames.join(' ')} onClick={onClick} type="button">
+      {getDate(day)}
     </button>
   )
 }
@@ -64,8 +65,6 @@ export interface CalendarSelectorProps {
 export const CalendarSelector = (props: CalendarSelectorProps): JSX.Element => {
   const [selectedDate, setSelectedDate] = useState(new Date())
 
-  const componentNode = useRef<HTMLElement>(null)
-
   const selectNewDate = (targetDate: Date): void => {
     setSelectedDate(targetDate)
     props.onSelectedDateChange?.(targetDate)
@@ -73,45 +72,24 @@ export const CalendarSelector = (props: CalendarSelectorProps): JSX.Element => {
 
   const goToToday = () => setSelectedDate(new Date())
 
-  const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-
-  const firstDayOfSelectedMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1)
-
-  const getPreviousMonday = (startDate: Date): Date => {
-    const dayOfWeek = startDate.getDay()
-    if (dayOfWeek === 1) {
-      return startDate
-    }
-
-    const newDate = startDate
-    if (dayOfWeek === 0) {
-      newDate.setDate(startDate.getDate() - 6)
-    } else {
-      newDate.setDate(startDate.getDate() - (startDate.getDay() - 1))
-    }
-
-    return newDate
-  }
-
-  const daysToRender = new Array<Date>()
-  const start = getPreviousMonday(firstDayOfSelectedMonth)
-  for (let index = 0; index < 35; index++) {
-    const dayToAdd = new Date(start.getTime() + index * 24 * 60 * 60 * 1000)
-    daysToRender.push(dayToAdd)
-  }
+  const monthStart = startOfMonth(selectedDate)
+  const monthEnd = endOfMonth(selectedDate)
+  const startFirstWeek = startOfWeek(monthStart, { weekStartsOn: 1 })
+  const endLastWeek = endOfWeek(monthEnd, { weekStartsOn: 1 })
+  const daysToRender = eachDayOfInterval({ start: startFirstWeek, end: endLastWeek })
 
   const gotoPreviousMonth = () => {
-    setSelectedDate((oldDate) => new Date(oldDate.getFullYear(), oldDate.getMonth() - 1, 1))
+    setSelectedDate((oldDate) => subMonths(oldDate, 1))
   }
 
   const gotoNextMonth = () => {
-    setSelectedDate((oldDate) => new Date(oldDate.getFullYear(), oldDate.getMonth() + 1, 1))
+    setSelectedDate((oldDate) => addMonths(oldDate, 1))
   }
 
-  const monthTitle = getMonthTitle(selectedDate)
+  const monthTitle = format(selectedDate, 'MMM yyyy')
 
   return (
-    <section className={props.className} ref={componentNode}>
+    <section className={props.className}>
       <Popover className="flex justify-center">
         <Popover.Button>
           <CalendarIcon src={calendarIcon} childPosition="right">
@@ -133,7 +111,7 @@ export const CalendarSelector = (props: CalendarSelectorProps): JSX.Element => {
         >
           <Popover.Panel className="absolute z-10">
             {({ close }) => (
-              <section className="text-sm absolute border-2 bg-gray-200 w-80 h-64 rounded-xl p-2 ">
+              <section className="text-sm absolute border-2 bg-gray-200 w-80 rounded-xl p-2 ">
                 <header className="flex justify-between p-0 pb-2 font-bold">
                   <CalendarIcon title="Goto previous month" onClick={gotoPreviousMonth} src={backIcon} size={20} />
                   <CalendarIcon title="Goto today" onClick={goToToday} src={home} size={20}>
@@ -141,26 +119,21 @@ export const CalendarSelector = (props: CalendarSelectorProps): JSX.Element => {
                   </CalendarIcon>
                   <CalendarIcon title="Goto next month" onClick={gotoNextMonth} src={forwardIcon} size={20} />
                 </header>
-                <div className="grid grid-cols-7 gap-3">
-                  {weekDays.map((weekDay, index) => {
-                    const classNames = ['p-1 text-center border-gray-800 border-b']
-                    if (index > 4) {
-                      classNames.push('text-green-600')
-                    }
-
-                    return (
-                      <div className={classNames.join(' ')} key={index}>
-                        {weekDay}
-                      </div>
-                    )
-                  })}
-                  {daysToRender.map((day, index) => (
+                <div className="grid grid-cols-7 grid-flow-row gap-3 auto-cols-min">
+                  <div className="p-1 text-center border-gray-800 border-b">Mon</div>
+                  <div className="p-1 text-center border-gray-800 border-b">Tue</div>
+                  <div className="p-1 text-center border-gray-800 border-b">Wed</div>
+                  <div className="p-1 text-center border-gray-800 border-b">Thu</div>
+                  <div className="p-1 text-center border-gray-800 border-b">Fri</div>
+                  <div className="p-1 text-center border-gray-800 border-b text-green-600">Sat</div>
+                  <div className="p-1 text-center border-gray-800 border-b text-green-600">Sun</div>
+                  {daysToRender.map((day) => (
                     <DayItem
-                      key={index}
+                      key={day.toString()}
                       day={day}
                       selectedDate={selectedDate}
-                      onClick={(date) => {
-                        selectNewDate(date)
+                      onClick={() => {
+                        selectNewDate(day)
                         close()
                       }}
                     />
