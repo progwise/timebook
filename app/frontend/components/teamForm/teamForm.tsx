aee542bf2a8829a521ecad8b7755eb3c4a73b746
Slash52 <91463734+Slash52@users.noreply.github.com>
2021-12-19 19:41:48 +0100

    fix esLint problem

--- app/frontend/components/teamForm/teamForm.tsx
+++ app/frontend/components/teamForm/teamForm.tsx
@@ -1,20 +1,20 @@
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { InputField } from '../inputField/inputField'
 
 export const TeamForm = (props: any) => {
   const [slug, setSlug] = useState('')
   const [invitationLink, setInvitationLink] = useState('')
   console.log(slug)
-  const handleSlugChange = (e: any) => {
-    setSlug(e.target.value)
-    setInvitationLink(`https:/${e.target.value}/team/invite `)
+  const handleSlugChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSlug(event.target.value)
+    setInvitationLink(`https:/${event.target.value}/team/invite `)
   }
   return (
     <form>
       <label>
         Company:
-        <InputField variant="primary" name="tbCompany" placeholder="Please enter the companies name"></InputField>
+        <InputField variant="primary" name="tbCompany" placeholder="Please enter the companies name" />
       </label>
       <label>
         Slug
@@ -24,11 +24,11 @@ export const TeamForm = (props: any) => {
           placeholder="This team is accsessible on https://tb.com/[slug]"
           onChange={handleSlugChange}
           value={slug}
-        ></InputField>
+        />
       </label>
       <label>
         Invitation link
-        <InputField variant="primary" name="tbInvitationLink" value={invitationLink}></InputField>
+        <InputField variant="primary" name="tbInvitationLink" value={invitationLink} />
       </label>
     </form>
   )
