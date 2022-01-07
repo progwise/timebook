6209b27aaa36de8e9a7d4e37a40579d9eb406fc1
Slash52 <91463734+Slash52@users.noreply.github.com>
2022-01-07 15:31:45 +0100

    temp

--- app/frontend/components/teamForm/teamForm.tsx
+++ app/frontend/components/teamForm/teamForm.tsx
@@ -1,14 +1,15 @@
 import { useRouter } from 'next/router'
 import { ChangeEvent, useState } from 'react'
+import { Button } from '../button/button'
 import { InputField } from '../inputField/inputField'
 
 export const TeamForm = (props: any) => {
   const [slug, setSlug] = useState('')
   const [invitationLink, setInvitationLink] = useState('')
-  console.log(slug)
+  const handleSaveClick = () => console.log('handleSaveClick')
   const handleSlugChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSlug(event.target.value)
-    setInvitationLink(`https:/${event.target.value}/team/invite `)
+    setInvitationLink(`https://tb.com${event.target.value}/team/invite `)
   }
   return (
     <form>
@@ -30,6 +31,9 @@ export const TeamForm = (props: any) => {
         Invitation link
         <InputField variant="primary" name="tbInvitationLink" value={invitationLink} />
       </label>
+      <Button variant="primary" onClick={handleSaveClick}>
+        Save
+      </Button>
     </form>
   )
 }
