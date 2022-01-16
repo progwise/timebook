82162a476984356bef3121faf5b18ad8848e35a1
Pascal Helbig <pascal.helbig@progwise.net>
2022-01-16 13:15:04 +0100

    test(team-form): add tests

--- app/jest-setup.js
+++ app/jest-setup.js
@@ -2,6 +2,8 @@
 /* eslint-disable unicorn/prefer-module */
 
 require('@testing-library/jest-dom')
+require('whatwg-fetch')
+
 jest.mock('next/image', () => ({
   __esModule: true,
   default: () => {
