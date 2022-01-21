3add0a891e6523cf772baf1ec56090ef59ef2386
Slash52 <91463734+Slash52@users.noreply.github.com>
2022-01-21 18:02:36 +0100

    Add Teams single to Top Navigation (#188)
    * Add Teams single to Top Navigation
    
    * Update app/frontend/components/topNavigation/topNavigation.tsx
    
    Co-authored-by: Pascal Helbig <PascalHelbig@users.noreply.github.com>
    
    Co-authored-by: Pascal Helbig <PascalHelbig@users.noreply.github.com>

--- app/frontend/components/topNavigation/topNavigation.tsx
+++ app/frontend/components/topNavigation/topNavigation.tsx
@@ -1,10 +1,12 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { useTeamsQuery } from '../../generated/graphql'
 import { TopNavigationLink } from './topNavigationLink'
 
 export const TopNavigation = (): JSX.Element => {
   const session = useSession()
   const router = useRouter()
+  const [{ data: teamsData }] = useTeamsQuery()
   const teamSlug = router.query.teamSlug
   return (
     <nav className="md:container md:mx-auto flex justify-center">
@@ -13,6 +15,13 @@ export const TopNavigation = (): JSX.Element => {
       {teamSlug && <TopNavigationLink href={`/${teamSlug}/projects`}>Projects</TopNavigationLink>}
       <TopNavigationLink href="/reports">Reports</TopNavigationLink>
       <TopNavigationLink href={teamSlug ? `/${teamSlug}/team` : '/team'}>Team</TopNavigationLink>
+      {teamsData?.teams.map((team) => {
+        return (
+          <TopNavigationLink href={`/${team.slug}/team`} key={team.id}>
+            {team.title}
+          </TopNavigationLink>
+        )
+      })}
       {session.status === 'authenticated' ? (
         <TopNavigationLink onClick={() => signOut({ callbackUrl: '/' })}>Sign out</TopNavigationLink>
       ) : (
