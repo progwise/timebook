bc5e08aa2c42bba156e2d0eea250f7c2080ae808
Slash52 <91463734+Slash52@users.noreply.github.com>
2022-01-21 16:27:17 +0100

    Add Protected Page to Team Pages

--- app/pages/[teamSlug]/customers/index.tsx
+++ app/pages/[teamSlug]/customers/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { ProtectedPage } from '../../../frontend/components/protectedPage'
 import { useCustomersQuery } from '../../../frontend/generated/graphql'
 
 const CustomersPage = (): JSX.Element => {
@@ -7,20 +8,22 @@ const CustomersPage = (): JSX.Element => {
   const [{ data }] = useCustomersQuery({ variables: { slug }, pause: !router.isReady })
 
   return (
-    <article>
-      <table className="w-full">
-        <tr>
-          <th>Name</th>
-          <th>Customer-ID</th>
-        </tr>
-        {data?.teamBySlug.customers.map((customer) => (
-          <tr key={customer.id}>
-            <td>{customer.title}</td>
-            <td>{customer.id}</td>
+    <ProtectedPage>
+      <article>
+        <table className="w-full">
+          <tr>
+            <th>Name</th>
+            <th>Customer-ID</th>
           </tr>
-        ))}
-      </table>
-    </article>
+          {data?.teamBySlug.customers.map((customer) => (
+            <tr key={customer.id}>
+              <td>{customer.title}</td>
+              <td>{customer.id}</td>
+            </tr>
+          ))}
+        </table>
+      </article>
+    </ProtectedPage>
   )
 }
 
