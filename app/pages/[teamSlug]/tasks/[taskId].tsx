8742815402f385bc78893bcc91eabe7b1a5cc6d3
Yohannes <johnbrhane1@gmail.com>
2022-01-25 11:34:41 +0100

    Task update implemented

--- app/pages/[teamSlug]/tasks/[taskId].tsx
+++ app/pages/[teamSlug]/tasks/[taskId].tsx
@@ -1,19 +1,45 @@
 import { useRouter } from 'next/router'
 import { TaskForm } from '../../../frontend/components/taskForm/taskForm'
-import { useTaskQuery } from '../../../frontend/generated/graphql'
+import { TaskInput, useTaskQuery, useTaskUpdateMutation } from '../../../frontend/generated/graphql'
 
 const Task = (): JSX.Element => {
   const router = useRouter()
-  const { taskId } = router.query
-  const [{ data }] = useTaskQuery({
+  const { taskId, teamSlug } = router.query
+  const [{ data, fetching }] = useTaskQuery({
     variables: { taskId: taskId?.toString() ?? '' },
     pause: !router.isReady,
   })
-  const selectedTask = data?.task
+  const [, taskUpdate] = useTaskUpdateMutation()
 
+  if (fetching || !router.isReady) {
+    return <div>Loading</div>
+  }
+
+  if (!data) {
+    return <div>Failed to load task</div>
+  }
+
+  const { task } = data
+
+  const handleSubmit = async (formData: TaskInput) => {
+    try {
+      const result = await taskUpdate({
+        id: task.id,
+        data: formData,
+      })
+      if (result.error) {
+        throw new Error('graphql error')
+      }
+      await router.push(`/${teamSlug}/projects/${task.project.id}`)
+    } catch {}
+  }
+
+  const handleCancel = async () => {
+    await router.push(`/${teamSlug}/projects/${task.project.id}`)
+  }
   return (
     <article>
-      <TaskForm task={selectedTask} />
+      <TaskForm task={task} onSubmit={handleSubmit} onCancel={handleCancel} />
     </article>
   )
 }
