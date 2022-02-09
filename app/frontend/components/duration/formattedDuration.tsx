a0f10133468db8b109d746205eec0e5addf435d4
Michael Vogt <michael.vogt@progwise.net>
2022-02-09 17:02:30 +0100

    formatted duration with test

--- app/frontend/components/duration/formattedDuration.tsx
+++ app/frontend/components/duration/formattedDuration.tsx
@@ -4,7 +4,7 @@ interface FormattedDurationProps {
 }
 
 export const FormattedDuration: React.FC<FormattedDurationProps> = ({ minutes, title }) => {
-  const valueAsString = !minutes ? '0:00' : `${Math.floor(minutes / 60)}:${('0' + (minutes % 60)).slice(-2)}`
+  const valueAsString = !minutes ? '0:00' : `${Math.floor(minutes / 60)}:${(minutes % 60).toString().padStart(2, '0')}`
   return (
     <span title={title} className="font-mono">
       {valueAsString}
