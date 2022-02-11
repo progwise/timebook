40e2eabaf9e7421ef30b7f394b72c12b6687023a
Yohannes <johnbrhane1@gmail.com>
2022-02-11 17:56:53 +0100

    refactor/implemented-review-comments

--- app/frontend/components/topNavigation/topNavigation.tsx
+++ app/frontend/components/topNavigation/topNavigation.tsx
@@ -1,6 +1,5 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
-import { RiAccountPinCircleFill } from 'react-icons/ri'
 import { TeamSelect } from './teamSelect'
 import { TopNavigationLink } from './topNavigationLink'
 import Image from 'next/image'
@@ -10,7 +9,6 @@ export const TopNavigation = (): JSX.Element => {
   const router = useRouter()
   const teamSlug = router.query.teamSlug
 
-  session.data?.user.image
   return (
     <nav className="flex justify-center md:container md:mx-auto">
       <TopNavigationLink href="/home">Home</TopNavigationLink>
@@ -24,13 +22,17 @@ export const TopNavigation = (): JSX.Element => {
       ) : (
         <TopNavigationLink onClick={() => signIn('github')}>Sign in</TopNavigationLink>
       )}
-      <TopNavigationLink href={`/${teamSlug}/me`}>
-        {session.data?.user.image ? (
-          <Image className="rounded-full" width={30} height={30} src={session.data?.user.image} alt="Profile picture" />
-        ) : (
-          <RiAccountPinCircleFill className="text-2xl" />
-        )}
-      </TopNavigationLink>
+      {teamSlug && session.data?.user.image && (
+        <TopNavigationLink href={`/${teamSlug}/me`}>
+          <Image
+            className="rounded-full"
+            width={30}
+            height={30}
+            src={session.data?.user.image}
+            alt={session.data?.user.name ?? 'Profile picture'}
+          />
+        </TopNavigationLink>
+      )}
     </nav>
   )
 }
--- app/pages/[teamSlug]/me.page.tsx
+++ app/pages/[teamSlug]/me.page.tsx
@@ -1,30 +1,28 @@
-import { useRouter } from 'next/router'
 import { useMeQuery } from '../../frontend/generated/graphql'
 import Image from 'next/image'
 
 const MyProfilePage = (): JSX.Element => {
-  const router = useRouter()
-  const [{ data: userData }] = useMeQuery()
+  const [{ data: userData, fetching }] = useMeQuery()
 
-  if (!router.isReady) {
+  if (fetching) {
     return <div>Loading...</div>
   }
 
   return (
     <div>
       <div className="mt-3 flex flex-row items-center gap-3">
-        {userData?.user.image ? (
-          <Image className="rounded-full" width={90} height={90} src={userData?.user.image} alt="Profile picture" />
+        {userData?.user.image && userData?.user.name ? (
+          <Image className="rounded-full" width={90} height={90} src={userData?.user.image} alt={userData?.user.name} />
         ) : undefined}
         <h1 className="text-xl">{userData?.user.name}</h1>
       </div>
       <div className="mt-10">
         <h2 className="mb-5 font-bold">Projects</h2>
-        {userData?.user.projects.map((project) => (
-          <ul key={project.id}>
-            <li>{project.title}</li>
-          </ul>
-        ))}
+        <ul>
+          {userData?.user.projects.map((project) => (
+            <li key={project.id}>{project.title}</li>
+          ))}
+        </ul>
       </div>
     </div>
   )
