e6b6675a5c7323ec3fa1fee6c1d1a5a93763e76f
Slash52 <91463734+Slash52@users.noreply.github.com>
2022-02-15 21:19:50 +0100

    added Test for teamSwitch

--- app/frontend/mocks/mockServer.ts
+++ app/frontend/mocks/mockServer.ts
@@ -1,9 +1,10 @@
 import { setupServer } from 'msw/node'
 import { handlers } from './handlers'
 import { projectHandlers } from './projectHandlers'
+import { teamHandlers } from './teamHandlers'
 import { workhourHandlers } from './workhourHandlers'
 
-const allHandlers = [...handlers, ...workhourHandlers, ...projectHandlers]
+const allHandlers = [...handlers, ...workhourHandlers, ...projectHandlers, ...teamHandlers]
 export const mockServer = setupServer(...allHandlers)
 
 // eslint-disable-next-line no-console
