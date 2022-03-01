5e077b5758cf9ca629933ab74f6af21ab9bbdd78
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2022-03-01 19:46:39 +0100

    Squashed failing commits
    Update index.page.tsx
    
    Delete tsconfig.json
    
    Revert "Delete tsconfig.json"
    
    This reverts commit 085fbc202635a42f6da2601663238c3775ba6246.

--- app/pages/[teamSlug]/reports/index.page.tsx
+++ app/pages/[teamSlug]/reports/index.page.tsx
@@ -2,10 +2,8 @@ import ReportForm from '../../../frontend/components/reportForm/reportForm'
 
 const ReportsPage = (): JSX.Element => {
   return (
-    <div className="mt-3">
-      <div>
-        <ReportForm />
-      </div>
+    <div>
+      <ReportForm />
     </div>
   )
 }
