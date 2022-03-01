6ac8cde6a4c83b303e3445af51c8d1e2ec2def45
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2022-03-01 19:46:39 +0100

    Update index.page.tsx

--- app/pages/[teamSlug]/reports/index.page.tsx
+++ app/pages/[teamSlug]/reports/index.page.tsx
@@ -2,11 +2,9 @@ import ReportForm from '../../../frontend/components/reportForm/reportForm'
 
 const ReportsPage = (): JSX.Element => {
   return (
-    <div className="mt-3">
       <div>
         <ReportForm />
       </div>
-    </div>
   )
 }
 
