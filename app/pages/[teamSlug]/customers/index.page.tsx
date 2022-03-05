4807b84d2298cc1458cd83f2886ee0b51f644dd2
santeleifer <94182007+santeleifer@users.noreply.github.com>
2022-03-05 23:34:53 +0100

    created CUSTOMER delete and update (#255)
    * created customer delete and update
    
    * Delete temp.page.tsx
    
    * run-prettier
    
    * final-changes
    
    * run-prettier
    
    Co-authored-by: Pascal Helbig <pascal.helbig@progwise.net>

--- app/pages/[teamSlug]/customers/index.page.tsx
+++ app/pages/[teamSlug]/customers/index.page.tsx
@@ -1,16 +1,29 @@
 import { useRouter } from 'next/router'
+import { useMemo } from 'react'
 import { Button } from '../../../frontend/components/button/button'
 import { ProtectedPage } from '../../../frontend/components/protectedPage'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeadCell,
+  TableHeadRow,
+  TableRow,
+} from '../../../frontend/components/table/table'
 import { useCustomersQuery } from '../../../frontend/generated/graphql'
 
 const CustomersPage = (): JSX.Element => {
   const router = useRouter()
 
   const slug = router.query.teamSlug?.toString() ?? ''
-  const [{ data }] = useCustomersQuery({ variables: { slug }, pause: !router.isReady })
+  const context = useMemo(() => ({ additionalTypenames: ['Customer'] }), [])
+  const [{ data }] = useCustomersQuery({ variables: { slug }, pause: !router.isReady, context })
 
   const handleAddCustomer = async () => {
     await router.push(`/${slug}/customers/add`)
+  }
+  const handleCustomerDetails = async (customerId: string) => {
+    await router.push(`/${slug}/customers/${customerId}`)
   }
   return (
     <ProtectedPage>
@@ -22,18 +35,27 @@ const CustomersPage = (): JSX.Element => {
           </Button>
         </div>
 
-        <table className="w-full">
-          <tr>
-            <th>Name</th>
-            <th>Customer-ID</th>
-          </tr>
-          {data?.teamBySlug.customers.map((customer) => (
-            <tr key={customer.id}>
-              <td>{customer.title}</td>
-              <td>{customer.id}</td>
-            </tr>
-          ))}
-        </table>
+        <Table className="w-full">
+          <TableHeadRow>
+            <TableHeadCell>Name</TableHeadCell>
+            <TableHeadCell>Customer-ID</TableHeadCell>
+            <TableHeadCell>Details</TableHeadCell>
+          </TableHeadRow>
+          <TableBody>
+            {data?.teamBySlug.customers.map((customer) => (
+              <TableRow key={customer.id}>
+                <TableCell>{customer.title}</TableCell>
+                <TableCell>{customer.id}</TableCell>
+
+                <TableCell>
+                  <Button variant="primarySlim" onClick={() => handleCustomerDetails(customer.id)}>
+                    Details
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </article>
     </ProtectedPage>
   )
