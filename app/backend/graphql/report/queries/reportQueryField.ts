df4a77e75578565cbaa646d52977a33224668de8
Pascal Helbig <pascal.helbig@progwise.net>
2022-03-25 17:41:03 +0100

    add authorize to report query field

--- app/backend/graphql/report/queries/reportQueryField.ts
+++ app/backend/graphql/report/queries/reportQueryField.ts
@@ -16,4 +16,42 @@ export const reportQueryField = queryField('report', {
     to: arg({ type: 'Date' }),
   },
   resolve: (_source, arguments_) => arguments_,
+  authorize: async (_source, arguments_, context) => {
+    if (!context.teamSlug) {
+      throw new Error('No team slug found')
+    }
+    if (!context.session) {
+      throw new Error('No session found')
+    }
+    const project = await context.prisma.project.findFirst({
+      where: {
+        id: arguments_.projectId,
+        team: { slug: context.teamSlug },
+      },
+    })
+    if (!project) {
+      throw new Error('No project found')
+    }
+    const projectMember = await context.prisma.projectMembership.findFirst({
+      where: {
+        projectId: arguments_.projectId,
+        teamMembership: {
+          userId: context.session.user.id,
+        },
+      },
+    })
+    const teamMember = await context.prisma.teamMembership.findFirst({
+      where: {
+        team: { slug: context.teamSlug },
+        userId: context.session.user.id,
+      },
+    })
+    const isTeamAdmin: boolean = teamMember?.role === 'ADMIN'
+
+    if (!projectMember && !isTeamAdmin) {
+      throw new Error('Not authorized')
+    }
+
+    return true
+  },
 })
