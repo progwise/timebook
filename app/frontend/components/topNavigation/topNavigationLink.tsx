5dd3fca925904ade41acc0f63339d9421d7e94e6
santeleifer <santeleifer@icloud.com>
2022-04-01 12:21:24 +0200

    revised suggestions

--- app/frontend/components/topNavigation/topNavigationLink.tsx
+++ app/frontend/components/topNavigation/topNavigationLink.tsx
@@ -10,12 +10,11 @@ export interface TopNavigationLinkProps {
 
 export const TopNavigationLink = (props: TopNavigationLinkProps): JSX.Element => {
   const router = useRouter()
-  const teamSlug = router.query.teamSlug as string
 
   let classNames = 'mx-3 my-3 bg-transparent  py-1 px-4 font-semibold'
 
   classNames =
-    props.href && router.pathname.replace('[teamSlug]', teamSlug).startsWith(props.href)
+    props.href && router.asPath.startsWith(props.href)
       ? classNames + ' cursor-default text-blue-400 border-b-2 border-blue-400 '
       : classNames + ' cursor-pointer text-blue-400 hover:text-blue-500 hover:border-b-2 hover:border-blue-300  '
 
