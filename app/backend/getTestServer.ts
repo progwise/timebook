b9dd91e38169a0a2612c2613f322cf646f03e71e
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2022-05-11 09:04:28 +0200

    feat and test: workHourDeleteMutationField authorize and integration tests

--- app/backend/getTestServer.ts
+++ app/backend/getTestServer.ts
@@ -3,16 +3,23 @@ import { ApolloServer } from 'apollo-server-micro'
 import { Context } from './graphql/context'
 import { schema } from './graphql/schema'
 
-export const getTestServer = (options: { teamSlug?: string; prisma: PrismaClient }) =>
+export const getTestServer = (options: {
+  teamSlug?: string
+  prisma: PrismaClient
+  noSession?: boolean
+  userId?: string
+}) =>
   new ApolloServer({
     schema: schema,
     context: {
       prisma: options.prisma,
       teamSlug: options.teamSlug,
-      session: {
-        user: {
-          id: '1',
-        },
-      },
+      session: options.noSession
+        ? undefined
+        : {
+            user: {
+              id: options.userId ?? '1',
+            },
+          },
     } as Context,
   })
