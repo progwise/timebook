ffc683e3a4712c773fa814671363cdef896030a9
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2022-05-18 12:38:41 +0200

    fix: workHourUpdateMutationField authorization and test (#333)
    * fix/workHourUpdateMutationField authorization and test
    
    * fix: implement review comments

--- app/backend/graphql/workHour/mutations/workHourUpdateMutationField.ts
+++ app/backend/graphql/workHour/mutations/workHourUpdateMutationField.ts
@@ -1,4 +1,5 @@
 import { idArg, mutationField } from 'nexus'
+import { isTeamAdmin } from '../../isTeamAdmin'
 import { WorkHour } from '../workHour'
 import { WorkHourInput } from '../workHourInput'
 
@@ -9,15 +10,39 @@ export const workHourUpdateMutationField = mutationField('workHourUpdate', {
     id: idArg({ description: 'id of the work hour item' }),
     data: WorkHourInput,
   },
-  authorize: (_source, _arguments, context) => !!context.session?.user.id,
+  authorize: async (_source, arguments_, context) => {
+    //Verifying if there is a session or a team
+    if (!context.session || !context.teamSlug) {
+      return false
+    }
+    //verifying a task <- project <- team availability
+    const task = await context.prisma.task.findFirst({
+      where: {
+        id: arguments_.id,
+        project: { team: { slug: context.teamSlug } },
+      },
+    })
+
+    if (!task) {
+      return false
+    }
+
+    if (await isTeamAdmin(context)) {
+      return true
+    }
+
+    // Verifying project membership of the user
+    const projectMember = await context.prisma.projectMembership.findUnique({
+      where: { userId_projectId: { userId: context.session.user.id, projectId: task.projectId } },
+    })
+
+    return !!projectMember
+  },
+
   resolve: (_source, { id, data }, context) => {
     return context.prisma.workHour.update({
       where: { id },
-      data: {
-        comment: data.comment,
-        duration: data.duration,
-        taskId: data.taskId,
-      },
+      data,
     })
   },
 })
