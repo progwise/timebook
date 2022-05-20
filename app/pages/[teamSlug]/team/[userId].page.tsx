9f735911063b89ee51bce27ddd3543e0aca1e39c
santeleifer <santeleifer@icloud.com>
2022-05-20 15:13:33 +0200

    feat: create UserQuery and use in userId.page

--- app/pages/[teamSlug]/team/[userId].page.tsx
+++ app/pages/[teamSlug]/team/[userId].page.tsx
@@ -1,30 +1,29 @@
-import { useRouter } from 'next/router'
-import { useTeamQuery } from '../../../frontend/generated/graphql'
+import { useUserQuery } from '../../../frontend/generated/graphql'
 import Image from 'next/image'
 import { Button } from '../../../frontend/components/button/button'
+import { useRouter } from 'next/router'
+
 const UserDetailsPage = (): JSX.Element => {
   const router = useRouter()
-  const { userId } = router.query
-  const [{ data: teamData, fetching, error }] = useTeamQuery()
 
-  if (fetching) {
-    return <div>Loading...</div>
-  }
-  if (error) {
-    return <h1>User not found</h1>
-  }
+  const { userId } = router.query
+
+  const [{ data, error, fetching }] = useUserQuery({
+    pause: !router.isReady,
+    variables: { userId: userId?.toString() ?? '' },
+  })
 
   return (
     <>
       <article>
-        {teamData?.team.members.map((user) => (
-          <div key={user.id} className="flex justify-start pt-10">
-            <span className="pr-8 ">
-              {user.image ? <Image width={40} height={40} src={user.image} alt={'Profile picture'} /> : undefined}
-            </span>
-            <h1 className="text-4xl text-blue-400">{user.name}</h1>
-          </div>
-        ))}
+        <div className="flex justify-start pt-10">
+          <span className="pr-8 ">
+            {data?.user.image ? (
+              <Image width={40} height={40} src={data?.user.image} alt={'Profile picture'} />
+            ) : undefined}
+          </span>
+          <h1 className="text-4xl text-blue-400">{data?.user.name}</h1>
+        </div>
       </article>
       <div className="flex justify-between pt-20">
         <div className="flex justify-center">
