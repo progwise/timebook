cd790f4f92bb7dc32cab98ec197780ca3a548618
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2022-05-20 18:50:53 +0200

    fix: user can not create more that 10 teams plus a team with existing slug (#340)

--- app/backend/graphql/team/mutations/teamCreateMutationField.ts
+++ app/backend/graphql/team/mutations/teamCreateMutationField.ts
@@ -2,6 +2,8 @@ import { mutationField } from 'nexus'
 import { Team } from '../team'
 import { TeamInput } from '../teamInput'
 
+const MAX_NUMBER_OF_TEAMS = 10
+
 export const teamCreateMutationField = mutationField('teamCreate', {
   type: Team,
   description: 'Create a new team',
@@ -9,9 +11,31 @@ export const teamCreateMutationField = mutationField('teamCreate', {
     data: TeamInput,
   },
   authorize: (_source, _arguments, context) => !!context.session?.user.id,
-  resolve: (_source, { data: { title, slug, theme } }, context) => {
+  resolve: async (_source, { data: { title, slug, theme } }, context) => {
     if (!context.session?.user) {
       throw new Error('not authenticated')
+    }
+
+    //How many teams does the user have?
+    const userTeamCount = await context.prisma.teamMembership.count({
+      where: {
+        userId: context.session.user.id,
+      },
+    })
+
+    //User should not exceed 10 teams
+    if (userTeamCount >= MAX_NUMBER_OF_TEAMS) {
+      throw new Error('Too many teams')
+    }
+
+    //Available team slug
+    const team = await context.prisma.team.findUnique({
+      where: { slug },
+    })
+
+    //Is the team slug already taken?
+    if (team) {
+      throw new Error('Team slug already taken')
     }
 
     return context.prisma.team.create({
