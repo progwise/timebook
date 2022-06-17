101f7eeda3a9b8a6a0fa3950ce3592bcb5416e2c
santeleifer <santeleifer@icloud.com>
2022-06-17 15:39:20 +0200

    fix: some design changes

--- app/frontend/components/dayWeekSwitchButton.tsx
+++ app/frontend/components/dayWeekSwitchButton.tsx
@@ -5,31 +5,27 @@ interface DayWeekSwitchProps {
 }
 
 export const DayWeekSwitch = ({ selectedButton }: DayWeekSwitchProps) => {
-  const selectedLink = 'hover:cursor-default bg-blue-400 border-1 border border-blue-700  '
+  const selectedLink = 'cursor-default bg-blue-400 border-1 border border-blue-700  '
   const unselectedLink = 'bg-gray-300 text-black border border-gray-500 '
 
   return (
-    <>
-      <span className="">
-        <Link href={'/time/week'}>
-          <a
-            className={` gap-1 rounded-l-lg py-1  pr-2 pl-2 text-xs ${
-              selectedButton === 'day' ? selectedLink : unselectedLink
-            }`}
-          >
-            Week
-          </a>
-        </Link>
-        <Link href={'/time'}>
-          <a
-            className={` gap-1 rounded-r-lg py-1 pl-3 pr-3 text-xs ${
-              selectedButton === 'week' ? selectedLink : unselectedLink
-            }`}
-          >
-            Day
-          </a>
-        </Link>
-      </span>
-    </>
+    <span className="text-center text-xs">
+      <Link href="/time/week">
+        <a
+          className={`inline-block w-10 rounded-l-lg py-1 ${selectedButton === 'day' ? selectedLink : unselectedLink}`}
+        >
+          Week
+        </a>
+      </Link>
+      <Link href="/time">
+        <a
+          className={` inline-block w-10 rounded-r-lg py-1 ${
+            selectedButton === 'week' ? selectedLink : unselectedLink
+          }`}
+        >
+          Day
+        </a>
+      </Link>
+    </span>
   )
 }
