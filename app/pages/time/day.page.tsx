0df8cde057ba6b51edd9c0b7b23bb6111aadf259
Sante Leifer <santeleifer@icloud.com>
2022-08-19 11:01:38 +0200

    feat: create switcher for previous and next day

--- app/pages/time/day.page.tsx
+++ app/pages/time/day.page.tsx
@@ -3,8 +3,22 @@ import { ProtectedPage } from '../../frontend/components/protectedPage'
 
 import { Button } from '../../frontend/components/button/button'
 import { BiPlus } from 'react-icons/bi'
+import { useRouter } from 'next/router'
+import { addDays, format, parse, startOfWeek } from 'date-fns'
+import Link from 'next/link'
 
 const WeekTime = (): JSX.Element => {
+ 
+  const router = useRouter()
+
+  const urlDate = router.query.date?.toString()
+
+  const currentDate = urlDate ? parse(urlDate, 'yyyy-MM-dd', new Date()) : new Date()
+  const nextDay = addDays(currentDate, 1)
+  const previousDay = addDays(currentDate, -1)
+  const previousDayString = format(previousDay, 'yyyy-MM-dd')
+  const nextDayString = format(nextDay, 'yyyy-MM-dd')
+  const CurrentDayString = format(currentDate, 'iii MM-dd')
   return (
     <ProtectedPage>
       <div className="flex flex-col items-end">
@@ -13,7 +27,15 @@ const WeekTime = (): JSX.Element => {
         </Button>
       </div>
       <article className="timebook">
-        <DayWeekSwitch selectedButton="day" />
+      <Link href={`/time/day?date=${previousDayString}`}>
+          <a className={`k w-10 rounded-l-lg bg-gray-400 px-2 py-1`}>Last</a>
+        </Link>
+        <Link href={`/time/day?date=${nextDayString}`}>
+          <a className={` w-10 rounded-r-lg bg-gray-400 px-2 py-1`}>Next</a>
+        </Link>
+
+        <div>This day: {CurrentDayString}</div>
+       <DayWeekSwitch selectedButton="day" />
         <h2>Your timetable for day</h2>
       </article>
     </ProtectedPage>
