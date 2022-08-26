1eb420475c886c6d209deb406a4a6c62862efce6
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2022-08-26 12:36:45 +0200

    fix: ComboBox displayValue options

--- app/frontend/components/combobox/combobox.tsx
+++ app/frontend/components/combobox/combobox.tsx
@@ -31,6 +31,16 @@ export const ComboBox = <Option extends { id: string }>({
 }: ComboBoxProps<Option>): JSX.Element => {
   const [inputQuery, setInputQuery] = useState('')
 
+  const generateLabel = (option: Option | NewOption | NoOption) => {
+    if (option === 'newOption') {
+      return `Create "${inputQuery}"`
+    }
+    if (option === 'noOption') {
+      return noOptionLabel
+    }
+    return displayValue(option)
+  }
+
   const filteredOptions = options.filter((option) => {
     return displayValue(option).toLowerCase().includes(inputQuery.toLowerCase())
   })
@@ -69,9 +79,9 @@ export const ComboBox = <Option extends { id: string }>({
     >
       <div className="relative mt-1">
         <div className="relative w-full cursor-default overflow-hidden rounded bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-          <HUCombobox.Input<'input', Option | undefined>
+          <HUCombobox.Input<'input', Option | NoOption | NewOption>
             className="w-full rounded py-2 pl-3 text-sm leading-5 focus:ring-0 dark:border-white dark:bg-slate-800 dark:text-white"
-            displayValue={displayValue}
+            displayValue={generateLabel}
             onChange={(event) => setInputQuery(event.target.value)}
             onBlur={onBlur}
           />
@@ -81,42 +91,34 @@ export const ComboBox = <Option extends { id: string }>({
         </div>
         <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
           <HUCombobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {allOptions.map((option) => {
-              let label: string
-              if (option === 'newOption') {
-                label = `Create "${inputQuery}"`
-              } else if (option === 'noOption') {
-                label = noOptionLabel
-              } else {
-                label = displayValue(option)
-              }
-              return (
-                <HUCombobox.Option<'li', Option | NewOption | NoOption>
-                  key={typeof option === 'string' ? option : option.id}
-                  value={option}
-                  className={({ active }) =>
-                    `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
-                      active ? 'bg-indigo-600 text-white' : 'text-gray-900'
-                    }`
-                  }
-                >
-                  {({ selected, active }) => (
-                    <>
-                      <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{label}</span>
-                      {selected ? (
-                        <span
-                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                            active ? 'text-white' : 'text-indigo-600'
-                          }`}
-                        >
-                          <HiCheck className="h-5 w-5" aria-hidden="true" />
-                        </span>
-                      ) : undefined}
-                    </>
-                  )}
-                </HUCombobox.Option>
-              )
-            })}
+            {allOptions.map((option) => (
+              <HUCombobox.Option<'li', Option | NewOption | NoOption>
+                key={typeof option === 'string' ? option : option.id}
+                value={option}
+                className={({ active }) =>
+                  `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                    active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                  }`
+                }
+              >
+                {({ selected, active }) => (
+                  <>
+                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                      {generateLabel(option)}
+                    </span>
+                    {selected ? (
+                      <span
+                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                          active ? 'text-white' : 'text-indigo-600'
+                        }`}
+                      >
+                        <HiCheck className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    ) : undefined}
+                  </>
+                )}
+              </HUCombobox.Option>
+            ))}
           </HUCombobox.Options>
         </Transition>
       </div>
