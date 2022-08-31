3af02767d4d3f9a07a2b31d7e7aeeea35df8768f
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2022-08-31 10:35:19 +0200

    fix: prettier

--- app/frontend/components/combobox/combobox.tsx
+++ app/frontend/components/combobox/combobox.tsx
@@ -91,7 +91,6 @@ export const ComboBox = <TOption extends { id: string }>({
         </div>
         <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
           <HUCombobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none  dark:bg-slate-800 sm:text-sm">
-
             {allOptions.map((option) => (
               <HUCombobox.Option<'li', TOption | NewOption | NoOption>
                 key={typeof option === 'string' ? option : option.id}
