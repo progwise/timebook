a766e30c0421a9d96ac208a3020bfa8cdb4071cb
Michael Vogt <michael.vogt@progwise.net>
2022-09-05 10:23:18 +0200

    Migrate WorkHour table to unique day/user/task constraint

--- app/backend/prisma/migrations/20220902151808_workhours_unique_day/migration.sql
+++ app/backend/prisma/migrations/20220902151808_workhours_unique_day/migration.sql
@@ -1,8 +1,9 @@
-/*
-  Warnings:
+-- prepare data
+select max("id") as "id", max("updatedAt") as "updatedAt", "date", "userId", "taskId", sum("duration") as "duration", min("start") as "start", max("end") as "end" into temp table "temp_workhour" from  "WorkHour" group by "date", "userId", "taskId";
+delete from "WorkHour";
+insert into "WorkHour" ("id", "date", "userId", "taskId", "duration", "start", "end", "updatedAt")
+	select "id", "date", "userId", "taskId", "duration", "start", "end", "updatedAt" from "temp_workhour";
+drop table "temp_workhour";
 
-  - A unique constraint covering the columns `[date,userId,taskId]` on the table `WorkHour` will be added. If there are existing duplicate values, this will fail.
-
-*/
 -- CreateIndex
 CREATE UNIQUE INDEX "WorkHour_date_userId_taskId_key" ON "WorkHour"("date", "userId", "taskId");
