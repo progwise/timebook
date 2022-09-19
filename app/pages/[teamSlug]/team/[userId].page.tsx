e789e4a068a8d9d6b572ca5680f38a5148d6fd74
Alex <41703864+WINt1703@users.noreply.github.com>
2022-09-19 16:54:10 +0200

    fix: pretties

--- app/pages/[teamSlug]/team/[userId].page.tsx
+++ app/pages/[teamSlug]/team/[userId].page.tsx
@@ -53,69 +53,66 @@ const UserDetailsPage = (): JSX.Element => {
             <div className="w-full">
               <h1 className="text-4xl dark:text-blue-400">{data?.user.name}</h1>
 
-              <div className="border-gray-400  dark:border-blue-400 border my-1 " />
+              <div className="my-1  border border-gray-400 dark:border-blue-400 " />
 
-              <div className='flex justify-between'>
+              <div className="flex justify-between">
                 <h1 className="text-xl font-semibold text-gray-400">Team member</h1>
 
                 <div className="flex">
-                  {
-                    isAdmin &&
+                  {isAdmin && (
                     <>
-                      {
-                        data?.user.role === Role.Member ?
-                          <Button className="mr-4" variant="secondary" onClick={handleUpgradeClick}>
-                            Promote to admin
-                          </Button>
-                        :
-                          <Button className="mr-4" variant="secondary" onClick={handleDowngradeClick}>
-                            Demote to member
-                          </Button>
-                      }
+                      {data?.user.role === Role.Member ? (
+                        <Button className="mr-4" variant="secondary" onClick={handleUpgradeClick}>
+                          Promote to admin
+                        </Button>
+                      ) : (
+                        <Button className="mr-4" variant="secondary" onClick={handleDowngradeClick}>
+                          Demote to member
+                        </Button>
+                      )}
                     </>
-                  }
+                  )}
 
                   <Button variant="danger">Remove from team</Button>
                 </div>
               </div>
             </div>
           </div>
-          {
-            isAdmin ?
-              <>
-                <h1 className="text-xl font-semibold text-gray-400"> All Projects:</h1>
-                <ul>
-                  {allProjects?.projects.map((project) => (
-                    <li key={project.id} className="p-3">
-                      <span className=" inline-block w-32"> {project.title} </span>
-                      <Toggle
-                        disabled={!isAdmin}
-                        checked={data?.user.projects.some((userProject) => userProject.id === project.id) ?? false}
-                        onChange={(newValue) => {
-                          if (!data?.user) {
-                            return
-                          }
-                          if (newValue === false) {
-                            deleteProjectMembership({ projectID: project.id, userID: data?.user.id })
-                          } else {
-                            createProjectMembership({ projectID: project.id, userID: data?.user.id })
-                          }
-                        }}
-                      />
-                    </li>
-                  ))}
-                </ul>
-              </>
-              :
-              <>
-                <h1 className="text-xl font-semibold text-gray-400"> Assigned Projects:</h1>
-                <ul>
-                  {data?.user.projects.map((project) => (
-                    <li key={project.id}> {project.title}</li>
-                  ))}
-                </ul>
-              </>
-          }
+          {isAdmin ? (
+            <>
+              <h1 className="text-xl font-semibold text-gray-400"> All Projects:</h1>
+              <ul>
+                {allProjects?.projects.map((project) => (
+                  <li key={project.id} className="p-3">
+                    <span className=" inline-block w-32"> {project.title} </span>
+                    <Toggle
+                      disabled={!isAdmin}
+                      checked={data?.user.projects.some((userProject) => userProject.id === project.id) ?? false}
+                      onChange={(newValue) => {
+                        if (!data?.user) {
+                          return
+                        }
+                        if (newValue === false) {
+                          deleteProjectMembership({ projectID: project.id, userID: data?.user.id })
+                        } else {
+                          createProjectMembership({ projectID: project.id, userID: data?.user.id })
+                        }
+                      }}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            <>
+              <h1 className="text-xl font-semibold text-gray-400"> Assigned Projects:</h1>
+              <ul>
+                {data?.user.projects.map((project) => (
+                  <li key={project.id}> {project.title}</li>
+                ))}
+              </ul>
+            </>
+          )}
         </article>
         {fetching && <span>Loading...</span>}
         {error && <span className="text-red-600">{error.message}!</span>}
