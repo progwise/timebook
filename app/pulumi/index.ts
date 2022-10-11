ad67f18993ca1fa6effb0bb27778bf6d1b1d93c4
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2022-10-11 12:21:14 +0200

    fix: prettier, build and pulumi deploy

--- app/pulumi/index.ts
+++ app/pulumi/index.ts
@@ -1,36 +1,35 @@
-import * as digitalocean from "@pulumi/digitalocean";
-import * as dotenv from 'dotenv';
+import * as digitalocean from '@pulumi/digitalocean'
+import * as dotenv from 'dotenv'
 
 dotenv.config({ path: '../.env' })
 
-export const timebook = new digitalocean.App("timebook", {
-    spec: {
-        domainNames: [
-            { name: "timebook.pascalhelbig.de" },
-            { name: "timebook2.progwise.net" }
+export const timebook = new digitalocean.App('timebook', {
+  spec: {
+    domainNames: [{ name: 'timebook.pascalhelbig.de' }, { name: 'timebook2.progwise.net' }],
+    name: 'timebook',
+    region: 'fra1',
+    services: [
+      {
+        environmentSlug: 'node-js',
+        git: {
+          branch: 'main',
+          repoCloneUrl: 'https://github.com/progwise/timebook',
+        },
+        instanceCount: 1,
+        name: 'timebook',
+        instanceSizeSlug: 'basic-xxs',
+        sourceDir: '/app',
+        buildCommand: 'npm install && npm run build',
+        runCommand: 'npm run start',
+        envs: [
+          { key: 'NEXTAUTH_URL', value: 'timebook.pascalhelbig.de' },
+          { key: 'NEXTAUTH_SECRET', value: process.env.NEXTAUTH_SECRET, type: 'SECRET' },
+          { key: 'GITHUB_CLIENT_ID', value: process.env.GITHUB_CLIENT_ID },
+          { key: 'GITHUB_CLIENT_SECRET', value: process.env.GITHUB_CLIENT_SECRET, type: 'SECRET' },
+          { key: 'DATABASE_URL', value: '${db.DATABASE_URL}' },
         ],
-        name: "timebook",
-        region: "fra1",
-        services: [{
-            environmentSlug: "node-js",
-            git: {
-                branch: "main",
-                repoCloneUrl: "https://github.com/progwise/timebook",
-            },
-            instanceCount: 1,
-            name: "timebook",
-            instanceSizeSlug: "basic-xxs",
-            sourceDir: "/app",
-            buildCommand: "npm install && npm run build",
-            runCommand: "npm run start",
-            envs: [
-                { key: "NEXTAUTH_URL", value: "timebook.pascalhelbig.de" },
-                { key: "NEXTAUTH_SECRET", value: process.env.NEXTAUTH_SECRET, type: "SECRET" },
-                { key: "GITHUB_CLIENT_ID", value: process.env.GITHUB_CLIENT_ID },
-                { key: "GITHUB_CLIENT_SECRET", value: process.env.GITHUB_CLIENT_SECRET, type: "SECRET" },
-                { key: "DATABASE_URL", value: "${db.DATABASE_URL}" },
-            ]
-        }],
-        databases: [{ engine: "PG", name: "db" }],
-    },
-});
+      },
+    ],
+    databases: [{ engine: 'PG', name: 'db' }],
+  },
+})
