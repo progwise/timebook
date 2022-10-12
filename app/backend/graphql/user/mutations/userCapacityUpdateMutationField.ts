a162772ecca308385686146ebf9681f027bdd252
Alex <41703864+WINt1703@users.noreply.github.com>
2022-10-12 12:40:59 +0200

    fix: changed type

--- app/backend/graphql/user/mutations/userCapacityUpdateMutationField.ts
+++ app/backend/graphql/user/mutations/userCapacityUpdateMutationField.ts
@@ -1,4 +1,4 @@
-import { floatArg, idArg, mutationField } from 'nexus'
+import { intArg, idArg, mutationField } from 'nexus'
 import { User } from '../user'
 import { isTeamAdmin } from '../../isTeamAdmin'
 
@@ -7,7 +7,7 @@ export const userCapacityUpdateMutationField = mutationField('userCapacityUpdate
   description: 'Updated a user capacity hours',
   args: {
     userId: idArg({ description: 'Id of the user' }),
-    capacityMinutes: floatArg({ description: 'Capacity of minutes' }),
+    capacityMinutes: intArg({ description: 'Capacity of minutes' }),
   },
   authorize: async (_source, {}, context) => isTeamAdmin(context),
   resolve: async (_source, { userId, capacityMinutes }, context) => {
