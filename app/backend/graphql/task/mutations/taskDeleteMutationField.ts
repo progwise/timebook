baffca8c7fab36a59844eabae685850fb5aa0d60
Pascal Helbig <pascal.helbig@progwise.net>
2022-10-17 09:49:08 +0200

    refactor(graphql): remove team slug from task mutations

--- app/backend/graphql/task/mutations/taskDeleteMutationField.ts
+++ app/backend/graphql/task/mutations/taskDeleteMutationField.ts
@@ -2,11 +2,18 @@ import { builder } from '../../builder'
 import { prisma } from '../../prisma'
 
 builder.mutationField('taskDelete', (t) =>
-  t.withAuth({ isTeamAdmin: true }).prismaField({
+  t.withAuth({ isLoggedIn: true }).prismaField({
     type: 'Task',
     description: 'Delete a task',
     args: {
       id: t.arg.id({ description: 'id of the task' }),
+    },
+    authScopes: async (_source, { id }) => {
+      const task = await prisma.task.findUniqueOrThrow({
+        select: { project: { select: { teamId: true } } },
+        where: { id: id.toString() },
+      })
+      return { isTeamAdminByTeamId: task.project.teamId }
     },
     resolve: (query, _source, { id }) =>
       prisma.task.delete({
--- app/backend/graphql/task/mutations/taskUpdateMutationField.ts
+++ app/backend/graphql/task/mutations/taskUpdateMutationField.ts
@@ -1,14 +1,31 @@
+import { ForbiddenError } from 'apollo-server-core'
 import { builder } from '../../builder'
 import { prisma } from '../../prisma'
 import { TaskInput } from '../taskInput'
 
 builder.mutationField('taskUpdate', (t) =>
-  t.withAuth({ isTeamAdmin: true }).prismaField({
+  t.withAuth({ isLoggedIn: true }).prismaField({
     type: 'Task',
     description: 'Update a task',
     args: {
       id: t.arg.id({ description: 'id of the task' }),
       data: t.arg({ type: TaskInput }),
+    },
+    authScopes: async (_source, { id, data: { projectId } }) => {
+      const task = await prisma.task.findUniqueOrThrow({
+        select: { project: { select: { teamId: true } } },
+        where: { id: id.toString() },
+      })
+      const project = await prisma.project.findUniqueOrThrow({
+        select: { teamId: true },
+        where: { id: projectId.toString() },
+      })
+
+      if (task.project.teamId !== project.teamId) {
+        throw new ForbiddenError('It is not allowed to move a task to a different team')
+      }
+
+      return { isTeamAdminByTeamId: task.project.teamId }
     },
     resolve: (query, _source, { id, data: { title, projectId } }) =>
       prisma.task.update({
