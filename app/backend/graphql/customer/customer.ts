9a8921899f458fb3501b2f990feaebf33152e1ba
Pascal Helbig <pascal.helbig@progwise.net>
2022-10-17 14:01:52 +0200

    refactor(graphql): remove team slug from ModifyInterface

--- app/backend/graphql/customer/customer.ts
+++ app/backend/graphql/customer/customer.ts
@@ -1,5 +1,6 @@
 import { builder } from '../builder'
 import { ModifyInterface } from '../interfaces/modifyInterface'
+import { prisma } from '../prisma'
 
 export const Customer = builder.prismaObject('Customer', {
   select: {},
@@ -8,5 +9,21 @@ export const Customer = builder.prismaObject('Customer', {
     id: t.exposeID('id', { description: 'Identifier of the customer' }),
     title: t.exposeString('title', { description: 'Title of the customer' }),
     projects: t.relation('projects', { description: 'List of all customer projects' }),
+    canModify: t.withAuth({ isLoggedIn: true }).boolean({
+      description: 'Can the user modify the entity',
+      select: { teamId: true },
+      resolve: async (customer, _arguments, context) => {
+        const teamMembership = await prisma.teamMembership.findUnique({
+          select: { role: true },
+          where: {
+            userId_teamId: {
+              teamId: customer.teamId,
+              userId: context.session.user.id,
+            },
+          },
+        })
+        return teamMembership?.role === 'ADMIN'
+      },
+    }),
   }),
 })
