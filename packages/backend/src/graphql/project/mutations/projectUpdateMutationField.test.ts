f5e94143446eaf6c44b46fe58d9f0b7c798793d8
Pascal Helbig <pascal.helbig@progwise.net>
2022-11-21 09:47:01 +0100

    test(backend): use gql tag in backend tests

--- packages/backend/src/graphql/project/mutations/projectUpdateMutationField.test.ts
+++ packages/backend/src/graphql/project/mutations/projectUpdateMutationField.test.ts
@@ -1,3 +1,4 @@
+import { gql } from 'apollo-server-core'
 import { GraphQLError } from 'graphql'
 
 import { PrismaClient } from '@progwise/timebook-prisma'
@@ -6,7 +7,7 @@ import { getTestServer } from '../../../getTestServer'
 
 const prisma = new PrismaClient()
 
-const projectUpdateMutation = `
+const projectUpdateMutation = gql`
   mutation projectUpdateMutation($id: ID!, $data: ProjectInput!) {
     projectUpdate(id: $id, data: $data) {
       id
--- packages/backend/src/graphql/team/mutations/teamUnarchiveMutationField.test.ts
+++ packages/backend/src/graphql/team/mutations/teamUnarchiveMutationField.test.ts
@@ -1,3 +1,4 @@
+import { gql } from 'apollo-server-core'
 import { GraphQLError } from 'graphql'
 
 import { PrismaClient } from '@progwise/timebook-prisma'
@@ -6,7 +7,7 @@ import { getTestServer } from '../../../getTestServer'
 
 const prisma = new PrismaClient()
 
-const teamUnarchiveMutation = `
+const teamUnarchiveMutation = gql`
   mutation teamUnarchiveMutation($id: ID!) {
     teamUnarchive(teamId: $id) {
       id
