636df12f3691085dbd689a5dad80c0bc40cdd13e
Michael Vogt <michael.vogt@progwise.net>
2022-11-29 22:42:26 +0100

    removed prettier from turbo again

--- packages/ui/spinner.tsx
+++ packages/ui/spinner.tsx
@@ -14,7 +14,7 @@ export const Spinner = ({ size }: SpinnerProps): JSX.Element => {
   return (
     <CgSpinner
       size={size ? Size[size] : Size.small}
-      className="inline animate-spin  text-black-600 dark:text-blue-600"
+      className="text-black-600 inline  animate-spin dark:text-blue-600"
     />
   )
 }
