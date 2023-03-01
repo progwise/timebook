4114508b79f11b058a11abd6f88ed77953cc44ec
Pascal Helbig <pascal.helbig@progwise.net>
2023-03-01 13:45:45 +0100

    feat(time page): hold day of the week in URL

--- apps/web/pages/time/index.page.tsx
+++ apps/web/pages/time/index.page.tsx
@@ -1,18 +1,21 @@
-import { getWeek, getYear, parse, addDays, format } from 'date-fns'
-import { useMemo, useState } from 'react'
+import { useRouter } from 'next/router'
+import { format, startOfWeek, endOfWeek, isThisWeek } from 'date-fns'
+import { useMemo } from 'react'
 
 import { ProtectedPage } from '../../frontend/components/protectedPage'
 import { WeekTable } from '../../frontend/components/weekTable/weekTable'
 import { WeekSelector } from '../../frontend/components/weekSelector'
 import { useTimeTableQuery } from '../../frontend/generated/graphql'
 
-const today = new Date()
-const NUMBER_OF_DAYS = 7
+export interface TimePageProps {
+  day?: Date
+}
 
-const TimePage = () => {
-  const [week, setWeek] = useState({ year: getYear(today), week: getWeek(today) })
-  const startDate = parse(week.week.toString(), 'I', new Date(week.year, 1, 1))
-  const endDate = addDays(startDate, NUMBER_OF_DAYS - 1)
+const TimePage = (props: TimePageProps) => {
+  const router = useRouter()
+  const day = props.day ?? new Date()
+  const startDate = startOfWeek(day, { weekStartsOn: 1 })
+  const endDate = endOfWeek(day, { weekStartsOn: 1 })
 
   const timeTableContext = useMemo(() => ({ additionalTypenames: ['Project', 'Task', 'WorkHour'] }), [])
   const [{ data: timeTableData }] = useTimeTableQuery({
@@ -20,14 +23,14 @@ const TimePage = () => {
     context: timeTableContext,
   })
 
-  const handleWeekChange = (year: number, week: number) => {
-    setWeek({ year, week })
+  const handleWeekChange = (newDate: Date) => {
+    router.push(isThisWeek(newDate) ? '/time' : `/time/${format(newDate, 'yyyy-MM-dd')}`)
   }
 
   return (
     <ProtectedPage>
       <h2>Time entries</h2>
-      <WeekSelector onChange={handleWeekChange} />
+      <WeekSelector value={day} onChange={handleWeekChange} />
       {timeTableData?.projects && (
         <WeekTable tableData={timeTableData.projects} startDate={startDate} endDate={endDate} />
       )}
