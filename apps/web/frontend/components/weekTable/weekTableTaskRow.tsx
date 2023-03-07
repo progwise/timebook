2462537315062c9c61bc5fdf71f988ceee3976c8
Tony Schroeder <tony.schroeder@gmx.de>
2023-03-07 05:47:26 +0100

    feat(weekTableTaskRow): change of the disabled prop condition#624

--- apps/web/frontend/components/weekTable/weekTableTaskRow.tsx
+++ apps/web/frontend/components/weekTable/weekTableTaskRow.tsx
@@ -1,49 +1,42 @@
-import { FormattedDuration, Spinner, TableCell, TableRow } from '@progwise/timebook-ui'
-import { eachDayOfInterval, isSameDay, parseISO, format, isBefore, isAfter } from 'date-fns'
-import { ProjectFilter, TaskWithWorkHoursFragment, useMyProjectsQuery } from '../../generated/graphql'
+import { FormattedDuration, TableCell, TableRow } from '@progwise/timebook-ui'
+import { eachDayOfInterval, isSameDay, parseISO, isBefore, isAfter } from 'date-fns'
+import { TaskWithWorkHoursFragment } from '../../generated/graphql'
 import { WeekTableTaskDayCell } from './weekTableTaskDayCell'
 
 interface WeekTableTaskRowProps {
   interval: { start: Date; end: Date }
   task: TaskWithWorkHoursFragment
+  projectStart?: string | null
+  projectEnd?: string | null
 }
 
-export const WeekTableTaskRow = ({ interval, task }: WeekTableTaskRowProps) => {
+export const WeekTableTaskRow = ({ interval, task, projectStart, projectEnd }: WeekTableTaskRowProps) => {
   const taskDurations = task.workHours
     .map((workHour) => workHour.duration)
     .reduce((previous, current) => previous + current, 0)
 
-  const [{ data }] = useMyProjectsQuery({
-    variables: { from: format(new Date(), 'yyyy-MM-dd'), filter: ProjectFilter.All },
-  })
-
-  const currentProjekt = data?.projects.find((project) => project.id === task.project.id)
-  const startDay = new Date(currentProjekt?.startDate ?? '')
-  const endDate = new Date(currentProjekt?.endDate ?? '')
-
   return (
     <TableRow>
       <TableCell>{task.title}</TableCell>
-      {data &&
-        eachDayOfInterval(interval).map((day) => {
-          const isDayBefore = isBefore(day, startDay)
-          const isDayAfter = isAfter(day, endDate)
+      {eachDayOfInterval(interval).map((day) => {
+        const durations = task.workHours
+          .filter((workHour) => isSameDay(parseISO(workHour.date), day))
+          .map((workHour) => workHour.duration)
+        const duration = durations.reduce((previous, current) => previous + current, 0)
 
-          const durations = task.workHours
-            .filter((workHour) => isSameDay(parseISO(workHour.date), day))
-            .map((workHour) => workHour.duration)
-          const duration = durations.reduce((previous, current) => previous + current, 0)
-
-          return (
-            <WeekTableTaskDayCell
-              day={day}
-              disabled={isDayBefore || isDayAfter}
-              taskId={task.id}
-              duration={duration}
-              key={day.toDateString()}
-            />
-          )
-        })}
+        return (
+          <WeekTableTaskDayCell
+            day={day}
+            disabled={
+              (projectStart ? isBefore(day, new Date(projectStart)) : false) ||
+              (projectEnd ? isAfter(day, new Date(projectEnd)) : false)
+            }
+            taskId={task.id}
+            duration={duration}
+            key={day.toDateString()}
+          />
+        )
+      })}
       <TableCell>
         <FormattedDuration minutes={taskDurations} title="" />
       </TableCell>
