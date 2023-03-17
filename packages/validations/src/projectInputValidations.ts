03724be43238f2401402cf02c1f77a1752d63c2b
inpaceee <116308003+inpaceee@users.noreply.github.com>
2023-03-17 11:21:06 +0100

    feat: changes project title max character length (#702)

--- packages/validations/src/projectInputValidations.ts
+++ packages/validations/src/projectInputValidations.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const projectInputValidations = z.object({
-  title: z.string().trim().min(1, 'title is required').max(20, 'title is too long'),
+  title: z.string().trim().min(1, 'title is required').max(50, 'title is too long'),
   start: z.date().nullish(),
   end: z.date().nullish(),
 })
