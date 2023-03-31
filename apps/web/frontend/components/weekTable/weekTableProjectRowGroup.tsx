7ea5af69c0a71105491ef320c713fc6773f2ebc1
inpaceee <bas1cly2303@gmail.com>
2023-03-31 11:05:29 +0200

    feat: collapsable projects in week view

--- apps/web/frontend/components/weekTable/weekTableProjectRowGroup.tsx
+++ apps/web/frontend/components/weekTable/weekTableProjectRowGroup.tsx
@@ -1,6 +1,8 @@
 import { eachDayOfInterval, isToday } from 'date-fns'
+import { useState } from 'react'
+import { BiArrowFromBottom, BiArrowFromTop, BiArrowToBottom, BiArrowToTop, BiUpArrow } from 'react-icons/bi'
 
-import { TableCell, TableRow } from '@progwise/timebook-ui'
+import { TableBody, TableCell, TableRow } from '@progwise/timebook-ui'
 
 import { FragmentType, graphql, useFragment } from '../../generated/gql'
 import { classNameMarkDay } from './classNameMarkDay'
@@ -24,20 +26,24 @@ interface WeekTableProjectRowGroupProps {
 
 export const WeekTableProjectRowGroup = ({ interval, project: projectFragment }: WeekTableProjectRowGroupProps) => {
   const project = useFragment(WeekTableProjectRowGroupFragment, projectFragment)
+  const [isExpanded, setIsExpanded] = useState(true)
+
   return (
     <>
-      <TableRow>
-        <TableCell className="font-bold" colSpan={2}>
-          {project.title}
+      <TableRow onClick={() => setIsExpanded(!isExpanded)} className="cursor-pointer">
+        <TableCell colSpan={2}>
+          <div className="flex items-center gap-1 font-bold">
+            {project.title}
+            {isExpanded ? <BiArrowToTop /> : <BiArrowToBottom />}
+            {!isExpanded && `${project.tasks.length} tasks`}
+          </div>
         </TableCell>
         {eachDayOfInterval(interval).map((day) => (
           <TableCell key={day.toDateString()} className={isToday(day) ? classNameMarkDay : ''} />
         ))}
         <TableCell />
       </TableRow>
-      {project.tasks.map((task) => (
-        <WeekTableTaskRow interval={interval} task={task} key={task.id} />
-      ))}
+      {isExpanded && project.tasks.map((task) => <WeekTableTaskRow interval={interval} task={task} key={task.id} />)}
     </>
   )
 }
