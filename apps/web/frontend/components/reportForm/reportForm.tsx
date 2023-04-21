ca33a6015e247888be74e21ebc774cd2df4f5673
Tony Schroeder <tony.schroeder@gmx.de>
2023-04-21 05:09:42 +0200

    feat/#739 show loadingSpinner when loading

--- apps/web/frontend/components/reportForm/reportForm.tsx
+++ apps/web/frontend/components/reportForm/reportForm.tsx
@@ -4,7 +4,7 @@ import { Fragment, useMemo, useState } from 'react'
 import { BiPrinter } from 'react-icons/bi'
 import { useQuery } from 'urql'
 
-import { Button, FormattedDuration, ListboxWithUnselect } from '@progwise/timebook-ui'
+import { Button, FormattedDuration, ListboxWithUnselect, Spinner } from '@progwise/timebook-ui'
 
 import { graphql, useFragment } from '../../generated/gql'
 import { ProjectFilter } from '../../generated/gql/graphql'
@@ -77,14 +77,14 @@ export const ReportForm = () => {
   const fromString = formatISO(from, { representation: 'date' })
   const endString = formatISO(to, { representation: 'date' })
 
-  const [{ data: projectsData }] = useQuery({
+  const [{ fetching: loadProjects, data: projectsData }] = useQuery({
     query: ReportProjectsQueryDocument,
     variables: { from: fromString, filter: ProjectFilter.All, date: { year, month } },
   })
   const projects = useFragment(ReportProjectFragment, projectsData?.projects)
 
   const context = useMemo(() => ({ additionalTypenames: ['WorkHour'] }), [])
-  const [{ data: reportGroupedData }] = useQuery({
+  const [{ fetching: loadReportGrouped, data: reportGroupedData }] = useQuery({
     query: ReportQueryDocument,
     variables: {
       projectId: selectedProjectId ?? '',
@@ -112,30 +112,35 @@ export const ReportForm = () => {
         </div>
         <h2 className="print:hidden">Select a project</h2>
       </div>
+
       <div className="flex flex-col">
         <div className="flex justify-between">
-          <div className="flex flex-row items-start gap-4">
-            <ListboxWithUnselect
-              value={selectedProject}
-              getLabel={(project) => project.title}
-              getKey={(project) => project.id}
-              onChange={(project) => setSelectedProjectId(project?.id)}
-              options={projects ?? []}
-              noOptionLabel="Select Project"
-            />
-            <input
-              className="rounded-lg border-none py-2 pl-3 text-sm leading-5  shadow-md dark:bg-slate-700"
-              type="month"
-              value={format(date, 'yyyy-MM')}
-              onChange={(event) => {
-                if (event.target.value) {
-                  const newDate = parse(event.target.value, 'yyyy-MM', new Date())
-                  setDate(newDate)
-                }
-              }}
-            />
-            {selectedProject && <ProjectLockButton year={year} month={month} project={selectedProject} />}
-          </div>
+          {loadProjects ? (
+            <Spinner />
+          ) : (
+            <div className="flex flex-row items-start gap-4">
+              <ListboxWithUnselect
+                value={selectedProject}
+                getLabel={(project) => project.title}
+                getKey={(project) => project.id}
+                onChange={(project) => setSelectedProjectId(project?.id)}
+                options={projects ?? []}
+                noOptionLabel="Select Project"
+              />
+              <input
+                className="rounded-lg border-none py-2 pl-3 text-sm leading-5  shadow-md dark:bg-slate-700"
+                type="month"
+                value={format(date, 'yyyy-MM')}
+                onChange={(event) => {
+                  if (event.target.value) {
+                    const newDate = parse(event.target.value, 'yyyy-MM', new Date())
+                    setDate(newDate)
+                  }
+                }}
+              />
+              {selectedProject && <ProjectLockButton year={year} month={month} project={selectedProject} />}
+            </div>
+          )}
           <div>
             {selectedProjectId && (
               <ReportUserSelect
@@ -150,68 +155,76 @@ export const ReportForm = () => {
         </div>
 
         {selectedProject && (
-          <section className="mt-10 grid w-full grid-cols-3 gap-2 text-left">
-            <article className="contents border-y text-lg">
-              <hr className="col-span-3 -mb-2 h-0.5 bg-gray-600" />
-              <strong>Tasks</strong>
-              <strong>Person</strong>
-              <strong>Hours</strong>
-            </article>
-            {reportGroupedData?.report.groupedByDate.map((group) => (
-              <Fragment key={group.date}>
+          <>
+            {loadProjects ? (
+              <div className="mt-10 flex justify-center">
+                <Spinner size="medium" />
+              </div>
+            ) : (
+              <section className="mt-10 grid w-full grid-cols-3 gap-2 text-left">
+                <article className="contents border-y text-lg">
+                  <hr className="col-span-3 -mb-2 h-0.5 bg-gray-600" />
+                  <strong>Tasks</strong>
+                  <strong>Person</strong>
+                  <strong>Hours</strong>
+                </article>
+                {reportGroupedData?.report.groupedByDate.map((group) => (
+                  <Fragment key={group.date}>
+                    <article className="contents">
+                      <hr className="col-span-3 -mt-2 h-0.5 bg-gray-700 " />
+                      <strong className="col-span-2">{group.date}</strong>
+                      <FormattedDuration
+                        title="Total work hours of the day"
+                        minutes={group.workHours
+                          .map((WorkHourDuration) => WorkHourDuration.duration)
+                          .reduce((sum, duration) => duration + sum, 0)}
+                      />
+                    </article>
+                    {group.workHours.map((workHour) => (
+                      <article key={workHour.id} className="contents">
+                        <h1>{workHour.task.title}</h1>
+                        <span>{workHour.user?.name}</span>
+                        <FormattedDuration title="Work duration" minutes={workHour.duration} />
+                      </article>
+                    ))}
+                  </Fragment>
+                ))}
                 <article className="contents">
-                  <hr className="col-span-3 -mt-2 h-0.5 bg-gray-700 " />
-                  <strong className="col-span-2">{group.date}</strong>
+                  <hr className="col-span-3 -mt-2 h-0.5 bg-gray-600" />
+                  <strong className="col-span-2">Total</strong>
                   <FormattedDuration
-                    title="Total work hours of the day"
-                    minutes={group.workHours
+                    title="Total work hours of the selected project"
+                    minutes={reportGroupedData?.report.groupedByDate
                       .map((WorkHourDuration) => WorkHourDuration.duration)
                       .reduce((sum, duration) => duration + sum, 0)}
                   />
                 </article>
-                {group.workHours.map((workHour) => (
-                  <article key={workHour.id} className="contents">
-                    <h1>{workHour.task.title}</h1>
-                    <span>{workHour.user?.name}</span>
-                    <FormattedDuration title="Work duration" minutes={workHour.duration} />
+                <article className="contents">
+                  <hr className="col-span-3 my-8 h-0.5 border-0 bg-gray-600" />
+                  <strong className="col-span-3">Total by Task</strong>
+                  {reportGroupedData?.report.groupedByTask.map((entry) => (
+                    <div key={entry.task.id} className="grid grid-flow-row">
+                      <span>{entry.task.title}</span>
+                      <FormattedDuration title="Total by task" minutes={entry.duration} />
+                    </div>
+                  ))}
+                  <hr className="col-span-3 my-8 h-0.5 border-0 bg-gray-600" />
+                </article>
+                {reportGroupedData?.report.groupedByUser && (
+                  <article className="contents">
+                    <strong className="col-span-3">Total by Person</strong>
+                    {reportGroupedData.report.groupedByUser.map((group) => (
+                      <div key={group.user.id} className="grid grid-flow-row">
+                        <span>{group.user.name}</span>
+                        <FormattedDuration title="Total by user" minutes={group.duration} />
+                      </div>
+                    ))}
+                    <hr className="col-span-3 -mt-2 h-0.5 bg-gray-600" />
                   </article>
-                ))}
-              </Fragment>
-            ))}
-            <article className="contents">
-              <hr className="col-span-3 -mt-2 h-0.5 bg-gray-600" />
-              <strong className="col-span-2">Total</strong>
-              <FormattedDuration
-                title="Total work hours of the selected project"
-                minutes={reportGroupedData?.report.groupedByDate
-                  .map((WorkHourDuration) => WorkHourDuration.duration)
-                  .reduce((sum, duration) => duration + sum, 0)}
-              />
-            </article>
-            <article className="contents">
-              <hr className="col-span-3 my-8 h-0.5 border-0 bg-gray-600" />
-              <strong className="col-span-3">Total by Task</strong>
-              {reportGroupedData?.report.groupedByTask.map((entry) => (
-                <div key={entry.task.id} className="grid grid-flow-row">
-                  <span>{entry.task.title}</span>
-                  <FormattedDuration title="Total by task" minutes={entry.duration} />
-                </div>
-              ))}
-              <hr className="col-span-3 my-8 h-0.5 border-0 bg-gray-600" />
-            </article>
-            {reportGroupedData?.report.groupedByUser && (
-              <article className="contents">
-                <strong className="col-span-3">Total by Person</strong>
-                {reportGroupedData.report.groupedByUser.map((group) => (
-                  <div key={group.user.id} className="grid grid-flow-row">
-                    <span>{group.user.name}</span>
-                    <FormattedDuration title="Total by user" minutes={group.duration} />
-                  </div>
-                ))}
-                <hr className="col-span-3 -mt-2 h-0.5 bg-gray-600" />
-              </article>
+                )}
+              </section>
             )}
-          </section>
+          </>
         )}
       </div>
     </>
--- apps/web/pages/week/index.page.tsx
+++ apps/web/pages/week/index.page.tsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/router'
 import { useMemo } from 'react'
 import { useQuery } from 'urql'
 
+import { Spinner } from '@progwise/timebook-ui'
+
 import { ProtectedPage } from '../../frontend/components/protectedPage'
 import { WeekSelector } from '../../frontend/components/weekSelector'
 import { WeekTable } from '../../frontend/components/weekTable/weekTable'
@@ -27,7 +29,7 @@ const WeekPage = (props: WeekPageProps) => {
   const endDate = endOfWeek(day, { weekStartsOn: 1 })
 
   const weekTableContext = useMemo(() => ({ additionalTypenames: ['Project', 'Task', 'WorkHour'] }), [])
-  const [{ data: weekTableData }] = useQuery({
+  const [{ fetching, data: weekTableData }] = useQuery({
     query: weekTableQueryDocument,
     variables: { from: format(startDate, 'yyyy-MM-dd'), to: format(endDate, 'yyyy-MM-dd') },
     context: weekTableContext,
@@ -41,8 +43,14 @@ const WeekPage = (props: WeekPageProps) => {
     <ProtectedPage>
       <h2>Week entries</h2>
       <WeekSelector value={day} onChange={handleWeekChange} />
-      {weekTableData?.projects && (
-        <WeekTable tableData={weekTableData.projects} startDate={startDate} endDate={endDate} />
+      {!fetching ? (
+        <div className="flex justify-center">
+          <Spinner size="medium" />
+        </div>
+      ) : (
+        weekTableData?.projects && (
+          <WeekTable tableData={weekTableData.projects} startDate={startDate} endDate={endDate} />
+        )
       )}
     </ProtectedPage>
   )
