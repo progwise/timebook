44741e6f7d71df81507f1c9853ad2e4ee48e8408
Pascal Helbig <pascal.helbig@progwise.net>
2023-12-15 16:26:42 +0100

    refactor: small cleanup

--- packages/backend/src/graphql/task/task.ts
+++ packages/backend/src/graphql/task/task.ts
@@ -69,12 +69,8 @@ export const Task = builder.prismaObject('Task', {
       select: { id: true },
       type: 'Tracking',
       nullable: true,
-      resolve: async (query, task, _argument, context) => {
-        const tracking = await prisma.tracking.findUnique({ ...query, where: { userId: context.session.user.id } })
-        if (tracking?.taskId === task.id) {
-          return tracking
-        }
-      },
+      resolve: async (query, task, _argument, context) =>
+        prisma.tracking.findUnique({ ...query, where: { userId: context.session.user.id, taskId: task.id } }),
     }),
     isLockedByUser: t.withAuth({ isLoggedIn: true }).boolean({
       select: { id: true },
