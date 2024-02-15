0dd2aa3d77a2ec1cfad779bb3841aff0cdbd2488
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2024-02-15 16:33:45 +0100

    feat: add avatars to project list (#859)
    * feat: add avatars to project list

--- apps/web/frontend/components/projectTable.tsx
+++ apps/web/frontend/components/projectTable.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { FragmentType, graphql, useFragment } from '../generated/gql'
@@ -8,12 +9,19 @@ export const ProjectTableItemFragment = graphql(`
     title
     startDate
     endDate
+    members {
+      id
+      image
+      name
+    }
   }
 `)
 
 interface ProjectTableProps {
   projects: FragmentType<typeof ProjectTableItemFragment>[]
 }
+
+const MAX_NUMBER_OF_AVATARS = 5
 
 export const ProjectTable = (props: ProjectTableProps): JSX.Element => {
   const projects = useFragment(ProjectTableItemFragment, props.projects)
@@ -24,16 +32,50 @@ export const ProjectTable = (props: ProjectTableProps): JSX.Element => {
         <thead className="text-lg text-base-content">
           <tr>
             <th>Name</th>
+            <th>Members</th>
             <th>Duration</th>
-            <th />
           </tr>
         </thead>
         <tbody className="text-base">
           {projects.map((project) => {
+            const numberOfMembersToBeDisplayed =
+              MAX_NUMBER_OF_AVATARS === project.members.length ? MAX_NUMBER_OF_AVATARS : MAX_NUMBER_OF_AVATARS - 1
             return (
               <tr key={project.id}>
                 <td>
                   <Link href={`/projects/${project.id}`}>{project.title}</Link>
+                </td>
+                <td>
+                  <div className="avatar-group -space-x-3">
+                    {project.members.slice(0, numberOfMembersToBeDisplayed).map((member) =>
+                      member.image ? (
+                        <div key={member.id} className="avatar">
+                          <div className="size-9">
+                            <Image
+                              key={member.id}
+                              width={36}
+                              height={36}
+                              src={member.image}
+                              alt={member.name ?? 'image of the user'}
+                            />
+                          </div>
+                        </div>
+                      ) : (
+                        <div key={member.id} className="avatar placeholder">
+                          <div className="size-9 rounded-full bg-neutral text-neutral-content">
+                            <span className="text-2xl">{member.name?.at(0)}</span>
+                          </div>
+                        </div>
+                      ),
+                    )}
+                    {project.members.length > numberOfMembersToBeDisplayed && (
+                      <div className="avatar placeholder">
+                        <div className="w-9 rounded-full bg-neutral text-neutral-content">
+                          <span>+{project.members.length - numberOfMembersToBeDisplayed}</span>
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </td>
                 <td>
                   {project.startDate} - {project.endDate}
