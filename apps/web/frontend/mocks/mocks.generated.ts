21607b7fb6939fd716d9081ee76ac89ccdf5eaa1
inpaceee <116308003+inpaceee@users.noreply.github.com>
2024-03-17 17:52:59 +0100

    fix: generated files

--- apps/web/frontend/mocks/mocks.generated.ts
+++ apps/web/frontend/mocks/mocks.generated.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 /* eslint-disable unicorn/prevent-abbreviations */
-import { GraphQLContext, GraphQLRequest, ResponseResolver, graphql } from 'msw'
+import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw'
 
 export type Maybe<T> = T | null
 export type InputMaybe<T> = Maybe<T>
@@ -743,7 +743,6 @@ export type SheetDayRowFragment = {
   __typename?: 'WorkHour'
   id: string
   duration: number
-  comment?: string
   project: { __typename?: 'Project'; title: string }
   task: { __typename?: 'Task'; title: string }
   user: { __typename?: 'User'; name?: string | null }
