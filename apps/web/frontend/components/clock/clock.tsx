acf239acea30a5ba44563c733b59eb8670851d59
inpaceee <116308003+inpaceee@users.noreply.github.com>
2024-04-09 18:04:38 +0200

    feat: adds 5 minute strokes, improves visuals

--- apps/web/frontend/components/clock/clock.tsx
+++ apps/web/frontend/components/clock/clock.tsx
@@ -34,53 +34,61 @@ export const Clock = () => {
       <style>{styleString}</style>
       <svg
         viewBox="0 0 40 40"
-        style={{
-          width: `24px`,
-          fill: `white`,
-          stroke: `black`,
-          strokeWidth: 2.5,
-          strokeLinecap: `round`,
-          transform: `rotate(-90deg)`,
-        }}
+        className="w-6 rotate-[-90deg] fill-base-100 stroke-base-content stroke-[1.5px]"
+        style={
+          {
+            '--start-seconds': currentTime.getSeconds(),
+            '--start-minutes': currentTime.getMinutes(),
+            '--start-hours': currentTime.getHours() % 12,
+            strokeLinecap: `round`,
+          } as React.CSSProperties
+        }
       >
         <circle cx="20" cy="20" r="18" />
+        <g className="translate-x-5 translate-y-5 stroke-[0.5px]">
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[30deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[60deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[90deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[120deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[150deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[180deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[210deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[240deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[270deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[300deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[330deg]" />
+          <line x1="15" y1="0" x2="16" y2="0" className="rotate-[360deg]" />
+        </g>
         <line
           x2="9"
-          style={
-            {
-              '--start-hours': currentTime.getHours() % 12,
-              transform: `transform(20px, 20px) rotate(0deg) translate(20px, 20px) rotate(calc(var(--start-hours) * 30deg))`,
-              strokeWidth: 4,
-              animation: `rotateHourHand calc(12 * 60 * 60s) linear infinite`,
-              animationDelay: `calc(calc(var(--start-minutes) * -60 * 1s) + calc(var(--start-seconds) * -1 * 1s))`,
-            } as React.CSSProperties
-          }
+          className="stroke-[4px]"
+          style={{
+            transform:
+              'transform(20px, 20px) rotate(0deg) translate(20px, 20px) rotate(calc(var(--start-hours) * 30deg))',
+            animation: 'rotateHourHand calc(12 * 60 * 60s) linear infinite',
+            animationDelay: 'calc(calc(var(--start-minutes) * -60 * 1s) + calc(var(--start-seconds) * -1 * 1s))',
+          }}
         />
         <line
           x2="13"
-          style={
-            {
-              '--start-minutes': currentTime.getMinutes(),
-              transform: `transform(20px, 20px) rotate(0deg) translate(20px, 20px) rotate(calc(var(--start-minutes) * 6deg))`,
-              strokeWidth: 2,
-              animation: `rotateMinuteHand 3600s steps(60) infinite`,
-              animationDelay: `calc(var(--start-seconds) * -1 * 1s)`,
-            } as React.CSSProperties
-          }
+          className="stroke-2"
+          style={{
+            transform:
+              'transform(20px, 20px) rotate(0deg) translate(20px, 20px) rotate(calc(var(--start-minutes) * 6deg))',
+            animation: 'rotateMinuteHand 3600s steps(60) infinite',
+            animationDelay: 'calc(var(--start-seconds) * -1 * 1s)',
+          }}
         />
         <line
           x2="16"
-          style={
-            {
-              '--start-seconds': currentTime.getSeconds(),
-              transform: `transform(20px, 20px) rotate(0deg) translate(20px, 20px) rotate(calc(var(--start-seconds) * 6deg))`,
-              strokeWidth: 1.5,
-              stroke: `red`,
-              animation: `rotateSecondsHand 60s steps(60) infinite`,
-            } as React.CSSProperties
-          }
+          className="stroke-primary stroke-[1.5px]"
+          style={{
+            transform:
+              'transform(20px, 20px) rotate(0deg) translate(20px, 20px) rotate(calc(var(--start-seconds) * 6deg))',
+            animation: 'rotateSecondsHand 60s steps(60) infinite',
+          }}
         />
-        <circle cx="20" cy="20" r="1.5" style={{ stroke: `red`, strokeWidth: 1.5 }} />
+        <circle cx="20" cy="20" r="1.5" className="stroke-primary stroke-[1.5px]" />
       </svg>
     </>
   )
