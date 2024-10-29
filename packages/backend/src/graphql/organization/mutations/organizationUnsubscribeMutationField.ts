e7352693aa9c117cc13650cccd0184148f3947ea
inpaceee <116308003+inpaceee@users.noreply.github.com>
2024-10-30 00:32:43 +0100

    feat: adds unsubscribe mutation

---
+++ packages/backend/src/graphql/organization/mutations/organizationUnsubscribeMutationField.ts
@@ -0,0 +1,23 @@
+/* eslint-disable unicorn/no-null */
+import { builder } from '../../builder'
+import { prisma } from '../../prisma'
+
+builder.mutationField('organizationUnsubscribe', (t) =>
+  t.prismaField({
+    type: 'Organization',
+    description: 'Unsubscribe from an organization',
+    args: {
+      organizationId: t.arg.id(),
+    },
+    authScopes: (_source, { organizationId }) => ({ isAdminByOrganization: organizationId.toString() }),
+    resolve: async (query, _source, arguments_) => {
+      const organizationId = arguments_.organizationId.toString()
+
+      return prisma.organization.update({
+        ...query,
+        where: { id: organizationId },
+        data: { subscriptionExpiresAt: null },
+      })
+    },
+  }),
+)
