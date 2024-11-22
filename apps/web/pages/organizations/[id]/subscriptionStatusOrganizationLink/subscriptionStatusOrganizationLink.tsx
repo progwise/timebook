8deb05a52c7e4da80807e4f02f9aa96ce4e13116
Pascal Helbig <1886867+PascalHelbig@users.noreply.github.com>
2024-11-22 11:32:42 +0100

    refactor: move styling from SubscriptionStatusOrganizationLink to AlertLink (#951)

--- apps/web/pages/organizations/[id]/subscriptionStatusOrganizationLink/subscriptionStatusOrganizationLink.tsx
+++ apps/web/pages/organizations/[id]/subscriptionStatusOrganizationLink/subscriptionStatusOrganizationLink.tsx
@@ -1,33 +1,30 @@
 import { useRouter } from 'next/router'
-import { FaCircleCheck, FaCircleXmark, FaTriangleExclamation } from 'react-icons/fa6'
 
-import { StatusLink } from './statusLink'
+import { AlertLink } from './alertLink'
 
 export const SubscriptionStatusOrganizationLink = () => {
   const router = useRouter()
-  const { status } = router.query
+  const { status, id } = router.query
+  const closeAlertHref = `/organizations/${id}`
 
   switch (status) {
     case 'subscriptionSuccess':
       return (
-        <StatusLink className="alert-success">
-          <FaCircleCheck className="text-xl" />
-          <span>Your purchase has been confirmed! It may take a few minutes to update.</span>
-        </StatusLink>
+        <AlertLink closeHref={closeAlertHref} status="success">
+          Your purchase has been confirmed! It may take a few minutes to update.
+        </AlertLink>
       )
     case 'subscriptionError':
       return (
-        <StatusLink className="alert-error">
-          <FaCircleXmark className="text-xl" />
-          <span>There was an error processing your payment. Please try again.</span>
-        </StatusLink>
+        <AlertLink closeHref={closeAlertHref} status="error">
+          There was an error processing your payment. Please try again.
+        </AlertLink>
       )
     case 'subscriptionCancel':
       return (
-        <StatusLink className="alert-warning">
-          <FaTriangleExclamation className="text-xl" />
-          <span>Payment process was cancelled.</span>
-        </StatusLink>
+        <AlertLink closeHref={closeAlertHref} status="warning">
+          Payment process was cancelled.
+        </AlertLink>
       )
     default:
       // eslint-disable-next-line unicorn/no-null
