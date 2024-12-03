754647a263739920f73db731f5d58c9610bf07c2
inpaceee <116308003+inpaceee@users.noreply.github.com>
2024-12-03 17:54:07 +0100

    fix: change hourly rate type on invoice item in graphql

--- packages/backend/src/graphql/invoiceItem/invoiceItem.ts
+++ packages/backend/src/graphql/invoiceItem/invoiceItem.ts
@@ -7,7 +7,11 @@ export const InvoiceItem = builder.prismaObject('InvoiceItem', {
     start: t.expose('start', { type: 'DateTime', nullable: true }),
     end: t.expose('end', { type: 'DateTime', nullable: true }),
     duration: t.exposeInt('duration', { description: 'Duration of the invoice item in minutes' }),
-    hourlyRate: t.exposeFloat('hourlyRate', { description: 'Hourly rate for the invoice item', nullable: true }),
+    hourlyRate: t.expose('hourlyRate', {
+      type: 'Decimal',
+      description: 'Hourly rate for the invoice item',
+      nullable: true,
+    }),
     invoice: t.relation('invoice', {
       description: 'Invoice to which the invoice item belongs',
       resolve: async (query, invoiceItem) => {
