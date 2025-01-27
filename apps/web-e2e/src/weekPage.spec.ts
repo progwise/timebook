cf751149d7f962477a7104df47a0b387a361bed1
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2025-01-27 18:57:18 +0100

    Use for loop to wait for the keyboard presses

--- apps/web-e2e/src/weekPage.spec.ts
+++ apps/web-e2e/src/weekPage.spec.ts
@@ -35,14 +35,11 @@ test.describe('week page', () => {
     let currentHours = 0
 
     const textboxes = await taskRow.getByRole('textbox', { name: 'duration' }).all()
-    await Promise.all(
-      textboxes.map(async (textbox) => {
-        await textbox.fill('1:00')
-        await page.keyboard.press('Tab')
-        currentHours++
-      }),
-    )
-
+    for (const textbox of textboxes) {
+      await textbox.fill('1:00')
+      await page.keyboard.press('Tab')
+      currentHours++
+    }
     await expect(taskRow.getByText(`${currentHours}:00`)).toBeVisible()
   })
 
