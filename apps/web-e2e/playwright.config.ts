dfb91ba1453fd0d7397ca7db931debeec0827c49
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2025-01-28 14:12:49 +0100

    Reduce timeouts

--- apps/web-e2e/playwright.config.ts
+++ apps/web-e2e/playwright.config.ts
@@ -8,13 +8,13 @@ dotenvConfig({ path: '../../.env' })
 const config: PlaywrightTestConfig = {
   testDir: './src',
   /* Maximum time one test can run for. */
-  timeout: 60 * 1000,
+  timeout: 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 60 * 1000,
+    timeout: 30 * 1000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -29,7 +29,7 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 60 * 1000,
+    actionTimeout: 30 * 1000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
