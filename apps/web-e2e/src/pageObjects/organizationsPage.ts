661e81255e5cdf5194554d298d638cbc6ac323e6
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2025-01-30 19:08:05 +0100

    Remove custom reporting suppress

--- apps/web-e2e/src/pageObjects/organizationsPage.ts
+++ apps/web-e2e/src/pageObjects/organizationsPage.ts
@@ -3,7 +3,7 @@ import { Page, expect } from '@playwright/test'
 
 export const createOrganizationsPage = (page: Page) => {
   const gotoOrganizationPage = async () => {
-    await page.goto('http://localhost:3000/organizations')
+    await page.goto('http://localhost:3000/organizations', { waitUntil: 'load' })
     await page.getByRole('link', { name: 'New organization' }).click()
   }
 
