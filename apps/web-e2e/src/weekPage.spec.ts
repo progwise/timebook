6afdaa564443ebe1dcf61e9299484ea4fd9f267c
Yohannes Tesfay <80110269+YohannesTesfay@users.noreply.github.com>
2025-02-03 12:22:19 +0100

    Use patterns

--- apps/web-e2e/src/weekPage.spec.ts
+++ apps/web-e2e/src/weekPage.spec.ts
@@ -27,31 +27,34 @@ test.afterAll(async () => {
 })
 
 test.describe('week page', () => {
+  const baseUrl = 'http://localhost:3000/week'
+  const weekUrlPattern = /\/week\/\d{4}-\d{2}-\d{2}\?userId=\w+/
+
   test('displays the current month', async () => {
     const currentMonthString = format(new Date(), 'MMMM')
-    await page.goto('http://localhost:3000/week')
+    await page.goto(`${baseUrl}`)
     const header = page.getByRole('heading', { name: currentMonthString }).nth(0)
 
     await expect(header).toBeVisible()
   })
 
   test('changes to the next week', async () => {
-    await page.goto('http://localhost:3000/week')
+    await page.goto(`${baseUrl}`)
 
     await page.getByRole('button', { name: 'Next week' }).click()
-    await expect(page).toHaveURL(/\/week\/\d{4}-\d{2}-\d{2}\?userId=\w+/)
+    await expect(page).toHaveURL(weekUrlPattern)
   })
 
   test('changes to the previous week', async () => {
-    await page.goto('http://localhost:3000/week')
+    await page.goto(`${baseUrl}`)
     await page.getByRole('button', { name: 'Previous week' }).click()
 
-    await expect(page).toHaveURL(/\/week\/\d{4}-\d{2}-\d{2}\?userId=\w+/)
+    await expect(page).toHaveURL(weekUrlPattern)
   })
 
   test('enters work hours', async () => {
     test.setTimeout(60 * 1000)
-    await page.goto('http://localhost:3000/week')
+    await page.goto(`${baseUrl}`)
 
     const taskRow = page.getByRole('row', { name: 'E2E Task' })
     await expect(taskRow).toBeVisible()
@@ -69,7 +72,7 @@ test.describe('week page', () => {
   })
 
   test('enters a comment', async () => {
-    await page.goto('http://localhost:3000/week')
+    await page.goto(`${baseUrl}`)
     await page.getByRole('button', { name: 'Comments' }).click()
 
     const commentBox = page.getByRole('textbox', { name: 'comment' }).first()
